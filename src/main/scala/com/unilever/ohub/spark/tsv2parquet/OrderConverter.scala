2e3d79b1557673cd2623e23d550445f08151f8e5
rodje1979 <githubjava@gmail.com>
2017-12-15 17:32:19 +0100

    Changed string into option[string]

--- src/main/scala/com/unilever/ohub/spark/tsv2parquet/OrderConverter.scala
+++ src/main/scala/com/unilever/ohub/spark/tsv2parquet/OrderConverter.scala
@@ -7,10 +7,10 @@ import org.apache.spark.sql.SaveMode.Overwrite
 import org.apache.spark.sql.SparkSession
 
 
-case class OrderRecord(REF_ORDER_ID:String, SOURCE:String, COUNTRY_CODE:String, STATUS:Option[Boolean], STATUS_ORIGINAL:String, REF_OPERATOR_ID:String,
-                       REF_CONTACT_PERSON_ID:String, ORDER_TYPE:String, TRANSACTION_DATE:Option[Timestamp], TRANSACTION_DATE_ORIGINAL:String, REF_PRODUCT_ID:String, QUANTITY:Option[Long], QUANTITY_ORIGINAL:String,
-                       ORDER_LINE_VALUE:Option[BigDecimal], ORDER_LINE_VALUE_ORIGINAL:String, ORDER_VALUE:Option[BigDecimal], ORDER_VALUE_ORIGINAL:String, WHOLESALER:String, CAMPAIGN_CODE:String, CAMPAIGN_NAME:String,
-                       UNIT_PRICE:Option[BigDecimal], UNIT_PRICE_ORIGINAL:String, CURRENCY_CODE:String, DATE_CREATED:Option[Timestamp], DATE_MODIFIED:Option[Timestamp])
+case class OrderRecord(REF_ORDER_ID:Option[String], SOURCE:Option[String], COUNTRY_CODE:Option[String], STATUS:Option[Boolean], STATUS_ORIGINAL:Option[String], REF_OPERATOR_ID:Option[String],
+                       REF_CONTACT_PERSON_ID:Option[String], ORDER_TYPE:Option[String], TRANSACTION_DATE:Option[Timestamp], TRANSACTION_DATE_ORIGINAL:Option[String], REF_PRODUCT_ID:Option[String], QUANTITY:Option[Long], QUANTITY_ORIGINAL:Option[String],
+                       ORDER_LINE_VALUE:Option[BigDecimal], ORDER_LINE_VALUE_ORIGINAL:Option[String], ORDER_VALUE:Option[BigDecimal], ORDER_VALUE_ORIGINAL:Option[String], WHOLESALER:Option[String], CAMPAIGN_CODE:Option[String], CAMPAIGN_NAME:Option[String],
+                       UNIT_PRICE:Option[BigDecimal], UNIT_PRICE_ORIGINAL:Option[String], CURRENCY_CODE:Option[String], DATE_CREATED:Option[Timestamp], DATE_MODIFIED:Option[Timestamp])
 
 object OrderConverter extends App {
   if (args.length != 2) {
@@ -30,12 +30,14 @@ object OrderConverter extends App {
 
   import spark.implicits._
 
+  lazy val expectedPartCount = 19
+
   val startOfJob = System.currentTimeMillis()
   val lines = spark.read.textFile(inputFile)
 
-  def checkOrderType(orderType: String): String = {
+  def checkOrderType(orderType: String): Option[String] = {
     if (Seq("", "SSD", "TRANSFER", "DIRECT", "UNKNOWN", "MERCHANDISE", "SAMPLE", "EVENT", "WEB", "BIN", "OTHER").contains(orderType)) {
-      orderType
+      Some(orderType)
     } else {
       throw new IllegalArgumentException("Unknown ORDER_TYPE: " + orderType)
     }
@@ -45,32 +47,32 @@ object OrderConverter extends App {
     .filter(line => !line.isEmpty && !line.startsWith("REF_ORDER_ID"))
     .map(line => line.split("‰", -1))
     .map(lineParts => {
-      checkLineLength(lineParts, 19)
+      checkLineLength(lineParts, expectedPartCount)
       try {
         OrderRecord(
-          REF_ORDER_ID = lineParts(0),
-          SOURCE = lineParts(1),
-          COUNTRY_CODE = lineParts(2),
+          REF_ORDER_ID = parseStringOption(lineParts(0)),
+          SOURCE = parseStringOption(lineParts(1)),
+          COUNTRY_CODE = parseStringOption(lineParts(2)),
           STATUS = parseBoolOption(lineParts(3)),
-          STATUS_ORIGINAL = lineParts(3),
-          REF_OPERATOR_ID = lineParts(4),
-          REF_CONTACT_PERSON_ID = lineParts(5),
+          STATUS_ORIGINAL = parseStringOption(lineParts(3)),
+          REF_OPERATOR_ID = parseStringOption(lineParts(4)),
+          REF_CONTACT_PERSON_ID = parseStringOption(lineParts(5)),
           ORDER_TYPE = checkOrderType(lineParts(6)),
           TRANSACTION_DATE = parseDateTimeStampOption(lineParts(7)),
-          TRANSACTION_DATE_ORIGINAL = lineParts(7),
-          REF_PRODUCT_ID = lineParts(8),
+          TRANSACTION_DATE_ORIGINAL = parseStringOption(lineParts(7)),
+          REF_PRODUCT_ID = parseStringOption(lineParts(8)),
           QUANTITY = parseLongRangeOption(lineParts(9)),
-          QUANTITY_ORIGINAL = lineParts(9),
-          ORDER_LINE_VALUE = parseBigDecimalOption(lineParts(10)), //TODO should be BigDecimal
-          ORDER_LINE_VALUE_ORIGINAL = lineParts(10),
+          QUANTITY_ORIGINAL = parseStringOption(lineParts(9)),
+          ORDER_LINE_VALUE = parseBigDecimalOption(lineParts(10)),
+          ORDER_LINE_VALUE_ORIGINAL = parseStringOption(lineParts(10)),
           ORDER_VALUE = parseBigDecimalOption(lineParts(11)),
-          ORDER_VALUE_ORIGINAL = lineParts(11),
-          WHOLESALER = lineParts(12),
-          CAMPAIGN_CODE = lineParts(13),
-          CAMPAIGN_NAME = lineParts(14),
-          UNIT_PRICE = parseBigDecimalOption(lineParts(15)), //TODO should be BigDecimal
-          UNIT_PRICE_ORIGINAL = lineParts(15),
-          CURRENCY_CODE = lineParts(16),
+          ORDER_VALUE_ORIGINAL = parseStringOption(lineParts(11)),
+          WHOLESALER = parseStringOption(lineParts(12)),
+          CAMPAIGN_CODE = parseStringOption(lineParts(13)),
+          CAMPAIGN_NAME = parseStringOption(lineParts(14)),
+          UNIT_PRICE = parseBigDecimalOption(lineParts(15)),
+          UNIT_PRICE_ORIGINAL = parseStringOption(lineParts(15)),
+          CURRENCY_CODE = parseStringOption(lineParts(16)),
           DATE_CREATED = parseDateTimeStampOption(lineParts(17)),
           DATE_MODIFIED = parseDateTimeStampOption(lineParts(18))
         )
@@ -79,7 +81,7 @@ object OrderConverter extends App {
       }
     })
 
-  records.write.mode(Overwrite).format("parquet").save(outputFile)
+  records.write.mode(Overwrite).partitionBy("COUNTRY_CODE").format("parquet").save(outputFile)
 
   records.printSchema()
 
