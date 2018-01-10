e56b1082cefa383b6b89fffd677f04bc3eb8ca5d
rodje1979 <githubjava@gmail.com>
2018-01-10 12:17:21 +0100

    Added full operator pipeline ready for testing in Azure

--- src/main/scala/com/unilever/ohub/spark/matching/OperatorMatching2.scala
+++ src/main/scala/com/unilever/ohub/spark/matching/OperatorMatching2.scala
@@ -3,7 +3,7 @@ package com.unilever.ohub.spark.matching
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import com.unilever.ohub.spark.tsv2parquet.StringFunctions
 
-object OperatorMatching extends App {
+object OperatorMatching2 extends App {
 
   if (args.length != 2) {
     println("specify INPUT_FILE OUTPUT_FILE")
