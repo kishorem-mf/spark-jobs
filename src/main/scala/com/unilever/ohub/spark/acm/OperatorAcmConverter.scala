9fcb579c83513a7ce980bf0aeacfa1e400345c8a
rodje1979 <githubjava@gmail.com>
2018-01-23 19:12:08 +0100

    Completed operator acm converter

---
+++ src/main/scala/com/unilever/ohub/spark/acm/OperatorAcmConverter.scala
@@ -0,0 +1,82 @@
+package com.unilever.ohub.spark.acm
+import org.apache.spark.sql.SparkSession
+import com.unilever.ohub.spark.generic.StringFunctions
+import org.apache.hadoop.fs.FileSystem
+
+object OperatorAcmConverter extends App{
+  if (args.length != 2) {
+    println("specify INPUT_FILE OUTPUT_FILE")
+    sys.exit(1)
+  }
+
+  val inputFile = args(0)
+  val outputFile = args(1)
+
+  println(s"Generating operator parquet from [$inputFile] to [$outputFile]")
+
+  val spark = SparkSession
+    .builder()
+    .appName(this.getClass.getSimpleName)
+    .getOrCreate()
+
+  import spark.implicits._
+  import org.apache.spark.sql.functions._
+  import com.unilever.ohub.spark.generic.SparkFunctions._
+
+  val startOfJob = System.currentTimeMillis()
+
+  val channelMappingDF = readJdbcTable(spark,dbTable = "channel_mapping")
+  val channelReferencesDF = readJdbcTable(spark,dbTable = "channel_references")
+  val allChannelMappingDF = channelMappingDF.join(channelReferencesDF,col("channel_reference_fk") === col("channel_reference_id"),"left")
+
+  spark.sqlContext.udf.register("CLEAN",(s1:String) => StringFunctions.removeGenericStrangeChars(s1 match {case null => null;case _ => s1}))
+  val operatorsInputDF = spark.read.parquet(inputFile)
+  allChannelMappingDF.createOrReplaceTempView("CHANNEL_MAPPING")
+  operatorsInputDF.createOrReplaceTempView("OPR_INPUT")
+
+//  TODO make sure OPR_LNKD_INTEGRATION_ID = filled with new group id + set GOLDEN_RECORD_FLAG to Y if golden record
+  val operatorFieldsParquetDF = spark.sql(
+    """
+      |select OPERATOR_CONCAT_ID OPR_ORIG_INTEGRATION_ID,null OPR_LNKD_INTEGRATION_ID,null GOLDEN_RECORD_FLAG,COUNTRY_CODE,
+      | clean(NAME) NAME,CHANNEL,SUB_CHANNEL,null ROUTE_TO_MARKET,REGION,OTM,
+      | clean(DISTRIBUTOR_NAME) PREFERRED_PARTNER,STREET,HOUSENUMBER HOUSE_NUMBER,ZIP_CODE ZIPCODE,
+      | clean(CITY) CITY,COUNTRY,
+      | round(AVG_PRICE,2) AVERAGE_SELLING_PRICE,NR_OF_DISHES NUMBER_OF_COVERS,
+      | case when 52 - WEEKS_CLOSED < 0 then null else 52 - WEEKS_CLOSED end NUMBER_OF_WEEKS_OPEN,DAYS_OPEN NUMBER_OF_DAYS_OPEN,CONVENIENCE_LEVEL,SALES_REP RESPONSIBLE_EMPLOYEE,NPS_POTENTIAL,null CAM_KEY,null CAM_TEXT,null CHANNEL_KEY,null CHANNEL_TEXT,CHAIN_ID CHAIN_KNOTEN,
+      | clean(CHAIN_NAME) CHAIN_NAME,null CUST_SUB_SEG_EXT,null CUST_SEG_EXT,null CUST_SEG_KEY_EXT,null CUST_GRP_EXT,null PARENT_SEGMENT,DATE_CREATED,DATE_MODIFIED DATE_UPDATED,
+      | case when STATUS = true then 'N' else 'Y' end DELETE_FLAG,DISTRIBUTOR_CUSTOMER_NR WHOLESALER_OPERATOR_ID,
+      | case when PRIVATE_HOUSEHOLD = false then 'N' else 'Y' end PRIVATE_HOUSEHOLD,VAT_NUMBER VAT,
+      | case when OPEN_ON_MONDAY = false then 'N' else 'Y' end OPEN_ON_MONDAY,
+      | case when OPEN_ON_TUESDAY = false then 'N' else 'Y' end OPEN_ON_TUESDAY,
+      | case when OPEN_ON_WEDNESDAY = false then 'N' else 'Y' end OPEN_ON_WEDNESDAY,
+      | case when OPEN_ON_THURSDAY = false then 'N' else 'Y' end OPEN_ON_THURSDAY,
+      | case when OPEN_ON_FRIDAY = false then 'N' else 'Y' end OPEN_ON_FRIDAY,
+      | case when OPEN_ON_SATURDAY = false then 'N' else 'Y' end OPEN_ON_SATURDAY,
+      | case when OPEN_ON_SUNDAY = false then 'N' else 'Y' end OPEN_ON_SUNDAY,
+      | clean(KITCHEN_TYPE) KITCHEN_TYPE,
+      | null LOCAL_CHANNEL,null CHANNEL_USAGE,null SOCIAL_COMMERCIAL,null STRATEGIC_CHANNEL,null GLOBAL_CHANNEL,null GLOBAL_SUBCHANNEL
+      |from OPR_INPUT
+    """.stripMargin)
+  operatorFieldsParquetDF.createOrReplaceTempView("OPR")
+
+  val ufsOperatorsDF = spark.sql(
+    """
+      |select OPR.OPR_ORIG_INTEGRATION_ID,OPR.OPR_LNKD_INTEGRATION_ID,OPR.GOLDEN_RECORD_FLAG,OPR.COUNTRY_CODE,OPR.NAME,OPR.CHANNEL,OPR.SUB_CHANNEL,OPR.ROUTE_TO_MARKET,OPR.REGION,OPR.OTM,OPR.PREFERRED_PARTNER,OPR.STREET,OPR.HOUSE_NUMBER,OPR.ZIPCODE,OPR.CITY,OPR.COUNTRY,OPR.AVERAGE_SELLING_PRICE,OPR.NUMBER_OF_COVERS,OPR.NUMBER_OF_WEEKS_OPEN,OPR.NUMBER_OF_DAYS_OPEN,OPR.CONVENIENCE_LEVEL,OPR.RESPONSIBLE_EMPLOYEE,OPR.NPS_POTENTIAL,OPR.CAM_KEY,OPR.CAM_TEXT,OPR.CHANNEL_KEY,OPR.CHANNEL_TEXT,OPR.CHAIN_KNOTEN,OPR.CHAIN_NAME,OPR.CUST_SUB_SEG_EXT,OPR.CUST_SEG_EXT,OPR.CUST_SEG_KEY_EXT,OPR.CUST_GRP_EXT,OPR.PARENT_SEGMENT,OPR.DATE_CREATED,OPR.DATE_UPDATED,OPR.DELETE_FLAG,OPR.WHOLESALER_OPERATOR_ID,OPR.PRIVATE_HOUSEHOLD,OPR.VAT,OPR.OPEN_ON_MONDAY,OPR.OPEN_ON_TUESDAY,OPR.OPEN_ON_WEDNESDAY,OPR.OPEN_ON_THURSDAY,OPR.OPEN_ON_FRIDAY,OPR.OPEN_ON_SATURDAY,OPR.OPEN_ON_SUNDAY,OPR.KITCHEN_TYPE,MPG.LOCAL_CHANNEL,MPG.CHANNEL_USAGE,MPG.SOCIAL_COMMERCIAL,MPG.STRATEGIC_CHANNEL,MPG.GLOBAL_CHANNEL,MPG.GLOBAL_SUBCHANNEL
+      |from OPR
+      |left join CHANNEL_MAPPING MPG
+      | on OPR.CHANNEL = MPG.ORIGINAL_CHANNEL
+    """.stripMargin)
+
+  ufsOperatorsDF.coalesce(1).write.option("encoding", "UTF-8").option("header", "true").option("delimiter","\u00B6").csv(outputFile)
+
+//  Rename file
+  import org.apache.hadoop.fs.{Path => hadoopPath}
+  val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+  val originalPath = new hadoopPath(s"$outputFile/part*")
+  val file = fs.globStatus(originalPath)(0).getPath
+  val dateTime = java.time.LocalDateTime.now().toString
+  val fileTimeStamp = s"${dateTime.substring(0,4)}${dateTime.substring(5,7)}${dateTime.substring(8,10)}${dateTime.substring(11,13)}${dateTime.substring(14,16)}${dateTime.substring(17,19)}"
+
+  fs.rename(new hadoopPath(s"${file.getParent}/${file.getName}"), new hadoopPath(s"${file.getParent.getParent}/UFS_OPERATORS_$fileTimeStamp.csv"))
+  fs.delete(new hadoopPath(s"${file.getParent}"), true)
+}
---
+++ src/main/scala/com/unilever/ohub/spark/generic/SparkFunctions.scala
@@ -0,0 +1,54 @@
+package com.unilever.ohub.spark.generic
+
+import java.util.Properties
+
+import org.apache.spark.sql.{DataFrame, SparkSession, functions}
+import org.apache.spark.sql.functions._
+import StringFunctions._
+import org.apache.spark.sql.types.StructType
+
+import scala.collection.SortedSet
+
+object SparkFunctions extends App{
+  private def testGetUniqueChars():Unit = {
+    val spark = SparkSession.builder().appName("SparkFunctions").getOrCreate()
+    val FILE_NAME = "C:\\out\\CONTACTPERSONS.parquet"
+    val FIELD_NAME = "LAST_NAME"
+    val inputDF = spark.read.parquet(FILE_NAME)
+    import spark.implicits._
+    val outputDF = addIdFieldToDataFrameField(spark,inputDF,FIELD_NAME)
+    outputDF.printSchema()
+    val dataFrameSize = inputDF.count()
+    val PART = 200000
+    val partitions = List.range(PART,dataFrameSize,PART)
+    var uniqueCharSet = scala.collection.mutable.SortedSet[Char]()
+    partitions.foreach(part =>
+      uniqueCharSet ++= getSetOfUniqueCharsInDataFrameColumn(outputDF.where("id between " + (part - PART) + " and " + part).select(FIELD_NAME)))
+    println(uniqueCharSet)
+  }
+
+  def addIdFieldToDataFrameField(spark:SparkSession, dataFrame:DataFrame, fieldName:String):DataFrame = {
+    import spark.implicits._
+    val dataFramePart = dataFrame.select(fieldName)
+    val dataFrameFull = dataFramePart.withColumn("ID",lit(1))
+    val dataSet = dataFramePart.as[String]
+    val rdd = dataSet.rdd
+    val rddWithId = rdd.zipWithUniqueId()
+    rddWithId.toDF(dataFrameFull.columns: _*)
+  }
+
+  def getSetOfUniqueCharsInDataFrameColumn(dataFrame:DataFrame):Set[Char] = {
+    val DF = dataFrame.distinct()
+    var myList = scala.collection.mutable.Set[String]()
+    DF.collect().foreach(row => myList.add(row.toString().replace("[","").replace("]","")))
+    collection.immutable.SortedSet(myList.mkString("").toCharArray.toList: _*)
+  }
+
+  def readJdbcTable(spark:SparkSession,dbConnectionString:String = "jdbc:postgresql://localhost:5432/",dbName:String = "ufs_example",dbTable:String,userName:String = "ufs_example",userPassword:String = "ufs_example"):DataFrame = {
+    val dbFullConnectionString = if(dbConnectionString.endsWith("/")) s"$dbConnectionString$dbName" else s"$dbConnectionString/$dbName"
+    val jdbcProperties = new Properties
+    jdbcProperties.put("user",userName)
+    jdbcProperties.put("password",userPassword)
+    spark.read.jdbc(dbFullConnectionString,dbTable,jdbcProperties)
+  }
+}
