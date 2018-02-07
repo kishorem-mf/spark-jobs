16eb156d8ac37998f6fd61b4d35dc15eb87f9d49
Tim van Cann <timvancann@godatadriven.com>
2018-02-07 16:15:38 +0100

    Update task names

--- ouniverse/new_leads.py
+++ ouniverse/new_leads.py
@@ -9,32 +9,32 @@ from spark_job_config import spark_cmd, ssh_hook
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 3, 1),
+    'start_date': datetime(2018, 2, 6),
     'email': ['airflow@airflow.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0
 }
 
-dag = DAG('new_leads', default_args=default_args)
+dag = DAG('new_leads', default_args=default_args, schedule_interval='0 0 1 * *')
 
 phase_one = BashOperator(
-    task_id="execute_bash_command",
+    task_id="phase_one",
     bash_command='echo "execute ouniverse phase I"',
     dag=dag)
 
 phase_two_grid = BashOperator(
-    task_id="execute_bash_command",
+    task_id="phase_two_grid",
     bash_command='echo "execute ouniverse phase II grid search"',
     dag=dag)
 
 phase_two_ids = BashOperator(
-    task_id="execute_bash_command",
+    task_id="phase_two_ids",
     bash_command='echo "execute ouniverse phase II id metadata"',
     dag=dag)
 
 prioritize = SSHExecuteOperator(
-    task_id="execute_bash_command",
+    task_id="prioritise_leads",
     bash_command=spark_cmd(jar='/ouniverse/universe-ingestion-spark-assembly-1.0.0-SNAPSHOT.jar',
                            main_class='com.unilever.ouniverse.leads.PrioritizeLeads',
                            args="--operators '*_output.avro' --places '*_details.avro' --leads 'cities.csv'\
--- ouniverse/update_metadata.py
+++ ouniverse/update_metadata.py
@@ -7,7 +7,7 @@ from airflow.operators.bash_operator import BashOperator
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 3, 1),
+    'start_date': datetime(2018, 2, 6),
     'email': ['airflow@airflow.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -17,21 +17,24 @@ default_args = {
 dag = DAG('ouniverse_update_metadata', default_args=default_args, schedule_interval="0 0 1 * *")
 
 remove_metadata_op = BashOperator(
-    task_id="execute_bash_command",
+    task_id="remove_metadata_operators",
     bash_command='echo "Remove metadata from operators"',
     dag=dag)
 
 remove_metadata_grid = BashOperator(
-    task_id="execute_bash_command",
+    task_id="remove_metadata_grid",
     bash_command='echo "Remove metadata from gridsearch result"',
     dag=dag)
 
 update_metadata_op = BashOperator(
-    task_id="execute_bash_command",
+    task_id="update_metadata_operators",
     bash_command='echo "Update metadata for operators"',
     dag=dag)
 
 update_metadata_grid = BashOperator(
-    task_id="execute_bash_command",
+    task_id="update_metadata_grid",
     bash_command='echo "Update metadata for gridsearch"',
     dag=dag)
+
+remove_metadata_op.set_downstream(update_metadata_op)
+remove_metadata_grid.set_downstream(update_metadata_grid)
