9fe9d4b1e9b6da6562170dbd894456535271abf5
Rodrigo Agundez <rragundez@gmail.com>
2018-02-26 12:33:24 +0100

    Change package name and PEP8

--- setup.py
+++ setup.py
@@ -3,24 +3,26 @@ from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 from setuptools import setup, Extension
 
-from sparse_dot_topn import __version__
+from string_matching import __version__
 
-ext_utils = Extension('sparse_dot_topn.sparse_dot_topn',
-                      sources=['./sparse_dot_topn/sparse_dot_topn.pyx', './sparse_dot_topn/sparse_dot_topn_source.cpp'],
-                      include_dirs=[numpy.get_include()],
-                      #libraries=[],
-                      extra_compile_args=['-std=c++0x', '-Os'],
-                      language='c++',
-                     )
+ext_utils = Extension(
+    'string_matching.sparse_dot_topn',
+    sources=['./string_matching/sparse_dot_topn.pyx',
+             './string_matching/sparse_dot_topn_source.cpp'],
+    include_dirs=[numpy.get_include()],
+    extra_compile_args=['-std=c++0x', '-Os'],
+    language='c++',
+)
 
-setup(name='sparse_dot_topn',
-      version=__version__,
-      setup_requires=[
-          # Setuptools 18.0 properly handles Cython extensions.
-          'setuptools>=18.0',
-          'cython',
-      ],
-      packages=['sparse_dot_topn'],
-      cmdclass={'build_ext': build_ext},
-      ext_modules=cythonize([ext_utils]),
-     )
+setup(
+    name='string_matching',
+    version=__version__,
+    setup_requires=[
+        # Setuptools 18.0 properly handles Cython extensions.
+        'setuptools>=18.0',
+        'cython',
+    ],
+    packages=['string_matching'],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=cythonize([ext_utils]),
+)
