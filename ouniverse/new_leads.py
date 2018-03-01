7c7be5525b57c8ac18b078585aad3eced59c097e
Tim van Cann <timvancann@godatadriven.com>
2018-03-01 15:37:57 +0100

    Fix hardcoded api value

--- ouniverse/new_leads.py
+++ ouniverse/new_leads.py
@@ -4,6 +4,7 @@ from airflow import DAG
 from airflow.contrib.operators.dataproc_operator import DataprocClusterCreateOperator, DataprocClusterDeleteOperator, \
     DataProcSparkOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.models import Variable
 
 default_args = {
     'owner': 'airflow',
@@ -24,7 +25,6 @@ cluster_defaults = {
 
 gs_jar_bucket = 'gs://ufs-prod/deployment/ouniverse'
 gs_data_bucket = 'gs://ufs-prod/data/raw/ouniverse'
-api_key = 'AIzaSyAYWgbn_awfrpfH4QrYQbUWQU0MAU1EbKI' # ugly ouch
 
 with DAG('new_leads', default_args=default_args,
          schedule_interval='@once') as dag:
@@ -46,7 +46,7 @@ with DAG('new_leads', default_args=default_args,
         gcp_conn_id=cluster_defaults['gcp_conn_id'],
         arguments=['--operators', f'{gs_data_bucket}/input/phase_I_NZ_sample.csv',
                    '--outputpath', f'{gs_data_bucket}/output/phaseI_output',
-                   '--apiKey', api_key])
+                   '--apiKey', Variable.get('google_api_key')])
 
     phase_two_grid = DataProcSparkOperator(
         task_id="phase_two_grid",
@@ -54,9 +54,9 @@ with DAG('new_leads', default_args=default_args,
         dataproc_spark_jars=[f'{gs_jar_bucket}/ouniverse-prioritisation-assembly-1.0.0-SNAPSHOT.jar'],
         cluster_name=cluster_defaults['cluster_name'],
         gcp_conn_id=cluster_defaults['gcp_conn_id'],
-        arguments=['--leads', f'{gs_data_bucket}/input/Phase_II_input_NZ_sample.csv',
+        arguments=['--leads', f'{gs_data_bucket}/input/Phase_II_Input_NZ_sample.csv',
                    '--outputpath', f'{gs_data_bucket}/output/phaseIIa_output',
-                   '--apiKey', api_key])
+                   '--apiKey', Variable.get('google_api_key')])
 
     phase_two_ids = DataProcSparkOperator(
         task_id="phase_two_ids",
@@ -68,7 +68,7 @@ with DAG('new_leads', default_args=default_args,
                    '--idColumn', 'placeId',
                    '--fileType', 'parquet',
                    '--outputpath', f'{gs_data_bucket}/output/phaseIIb_output',
-                   '--apiKey', api_key])
+                   '--apiKey', Variable.get('google_api_key')])
 
     prioritize = BashOperator(
         task_id="prioritise_leads",
