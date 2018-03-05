6cfe9ceb19d803587eb700e88b7cf5577384f8fa
DennisVis <dennisvis86@gmail.com>
2018-03-05 13:47:22 +0100

    Added correct params

--- acm_sftp_dag.py
+++ acm_sftp_dag.py
@@ -17,17 +17,40 @@ default_args = {
 
 acm_ssh_hook = SSHHook(ssh_conn_id='acm_sftp_ssh')
 
-dag = DAG('acm_sftp_dag', default_args=default_args,
-          schedule_interval="0 0 * * *")
+remote_filepath = "/incoming/OHUB_2_testing/quoted_semi_colon_delimited/"
+local_filepath = "/tmp/acm/{{ds}}/"
+templated_local_orderlines_filepath = local_filepath + "ORDERLINES.csv"
+templated_remote_orderlines_filepath = remote_filepath + "UFS_ORDERLINES_{{macros.ds_format(ds, '%Y%m%d')}}*.csv"
+templated_local_orders_filepath = local_filepath + "ORDERS.csv"
+templated_remote_orders_filepath = remote_filepath + "UFS_ORDERS_{{macros.ds_format(ds, '%Y%m%d')}}*.csv"
+templated_local_products_filepath = local_filepath + "PRODUCTS.csv"
+templated_remote_products_filepath = remote_filepath + "UFS_PRODUCTS_{{macros.ds_format(ds, '%Y%m%d')}}*.csv"
 
-templated_local_filepath = "/tmp/acm/{{ds}}/???"
-templated_remote_filepath = "/incoming/OHUB_2_testing/quoted_semi_colon_delimited/???"
+with DAG('acm_sftp_dag', default_args=default_args,
+          schedule_interval="0 0 * * *") as dag:
+    t1 = SFTPOperator(
+        task_id='Fetch ACM order lines for date',
+        dag=dag,
+        ssh_hook=acm_ssh_hook,
+        remote_host='unilever-z8i53y',
+        local_filepath=templated_local_orderlines_filepath,
+        remote_filepath=templated_remote_orderlines_filepath,
+        operation=SFTPOperation.GET)
 
-t1 = SFTPOperator(
-    task_id='Fetch ACM files for date',
-    dag=dag,
-    ssh_hook=acm_ssh_hook,
-    remote_host='unilever-z8i53y',
-    local_filepath=templated_local_filepath,
-    remote_filepath=templated_remote_filepath,
-    operation=SFTPOperation.GET)
+    t2 = SFTPOperator(
+        task_id='Fetch ACM orders for date',
+        dag=dag,
+        ssh_hook=acm_ssh_hook,
+        remote_host='unilever-z8i53y',
+        local_filepath=templated_local_orders_filepath,
+        remote_filepath=templated_remote_orders_filepath,
+        operation=SFTPOperation.GET)
+
+    t3 = SFTPOperator(
+        task_id='Fetch ACM products for date',
+        dag=dag,
+        ssh_hook=acm_ssh_hook,
+        remote_host='unilever-z8i53y',
+        local_filepath=templated_local_products_filepath,
+        remote_filepath=templated_remote_products_filepath,
+        operation=SFTPOperation.GET)
--- fuzzit_sftp_dag.py
+++ fuzzit_sftp_dag.py
@@ -16,18 +16,16 @@ default_args = {
 }
 
 fuzzit_ssh_hook = SSHHook(ssh_conn_id='fuzzit_sftp_ssh')
-
-dag = DAG('fuzzit_sftp_dag', default_args=default_args,
-          schedule_interval="0 0 * * *")
-
 templated_local_filepath = "/tmp/fuzzit/{{ds}}/UFS_Fuzzit_OHUB20_1400.zip"
 templated_remote_filepath = "./UFS_Fuzzit_OHUB20_{{macros.ds_format(ds, '%Y%m%d')}}_1400.zip"
 
-t1 = SFTPOperator(
-    task_id='Fetch Fuzzit files for date',
-    dag=dag,
-    ssh_hook=fuzzit_ssh_hook,
-    remote_host='apps.systrion.eu',
-    local_filepath=templated_local_filepath,
-    remote_filepath=templated_remote_filepath,
-    operation=SFTPOperation.GET)
+with DAG('fuzzit_sftp_dag', default_args=default_args,
+          schedule_interval="0 0 * * *") as dag:
+    t1 = SFTPOperator(
+        task_id='Fetch Fuzzit files for date',
+        dag=dag,
+        ssh_hook=fuzzit_ssh_hook,
+        remote_host='apps.systrion.eu',
+        local_filepath=templated_local_filepath,
+        remote_filepath=templated_remote_filepath,
+        operation=SFTPOperation.GET)
