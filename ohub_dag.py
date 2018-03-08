fc192a793b22649800d15b0dd0e3c62ded178e83
Tim van Cann <timvancann@godatadriven.com>
2018-03-08 08:49:07 +0100

    Add matching spark jobs to DAG

--- ohub_dag.py
+++ ohub_dag.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.contrib.operators.dataproc_operator import DataprocClusterCreateOperator, DataprocClusterDeleteOperator, \
-    DataProcSparkOperator
+    DataProcSparkOperator, DataProcPySparkOperator
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
@@ -16,7 +16,9 @@ default_args = {
     'pool': 'ohub-pool',
 }
 
-gs_jar_bucket = 'gs://ufs-prod/deployment/ohub2.0'
+gs_deployment = 'gs://ufs-prod/deployment/'
+gs_jar_bucket = f'{gs_deployment}/ohub2.0'
+gs_py_bucket = f'{gs_deployment}/name-matching'
 gs_data_input_bucket = 'gs://ufs-prod/data/raw/{}/**/*.csv'
 gs_data_output_bucket = 'gs://ufs-prod/data/parquet/{}.parquet'
 
@@ -41,13 +43,17 @@ csv_to_parquet = [
 
 to_acm = [
     {'class': 'OperatorConverter',
-     'input': 'OPERATORS'},
+     'output': 'operators',
+     'input': 'operators_merged'},
     {'class': 'OrderConverter',
-     'input': 'ORDERS'},
+     'output': 'orders',
+     'input': 'orders_merged'},
     {'class': 'ContactPersonConverter',
-     'input': 'CONTACTPERSONS'},
+     'output': 'contactpersons',
+     'input': 'contactpersons_merged_2'},
     {'class': 'ProductConverter',
-     'input': 'PRODUCTS'},
+     'output': 'products',
+     'input': 'products'},
 ]
 with DAG('ohub_dag', default_args=default_args,
          schedule_interval="0 0 * * *") as dag:
@@ -63,7 +69,7 @@ with DAG('ohub_dag', default_args=default_args,
         **cluster_defaults)
 
     for task in csv_to_parquet:
-        task_name = f"{task['input'].lower()}_to_csv"
+        task_name = f"{task['input'].lower()}_to_parquet"
         globals()[task_name] = DataProcSparkOperator(
             task_id=task_name,
             main_class=f"com.unilever.ohub.spark.tsv2parquet/{task['class']}",
@@ -83,5 +89,66 @@ with DAG('ohub_dag', default_args=default_args,
             cluster_name=cluster_defaults['cluster_name'],
             gcp_conn_id=cluster_defaults['gcp_conn_id'],
             arguments=[gs_data_input_bucket.format(task['input']),
-                       gs_data_output_bucket.format(task['input'].lower())])
+                       gs_data_output_bucket.format(task['output']) + '_acm'])
         globals()[task_name] >> delete_cluster
+
+    match_operators = DataProcPySparkOperator(
+        task_id='match_operators',
+        main=f'{gs_py_bucket}/match_operators.py',
+        py_files=[f'{gs_py_bucket}/dist/string_matching.egg'],
+        cluster_name=cluster_defaults['cluster_name'],
+        gcp_conn_id=cluster_defaults['gcp_conn_id'],
+        arguments=['--input_file', gs_data_output_bucket.format('operators'),
+                   '--output_path', gs_data_output_bucket.format('operators_matched')])
+
+    merge_operators = DataProcSparkOperator(
+        task_id='merge_operators',
+        main_class='com.unilever.ohub.spark.merging.OperatorMerging',
+        dataproc_spark_jars=cluster_defaults['jars'],
+        cluster_name=cluster_defaults['cluster_name'],
+        gcp_conn_id=cluster_defaults['gcp_conn_id'],
+        arguments=[gs_data_output_bucket.format('operators_matched'),
+                   gs_data_input_bucket.format('OPERATORS'),
+                   gs_data_output_bucket.format('operators_merged')])
+
+    globals()['operators_to_parquet'] >> match_operators >> merge_operators >> globals()['operators_to_acm']
+
+    merge_contactpersons1 = DataProcSparkOperator(
+        task_id='merge_contactpersons_1',
+        main_class='com.unilever.ohub.spark.merging.ContactPersonMerging1',
+        dataproc_spark_jars=cluster_defaults['jars'],
+        cluster_name=cluster_defaults['cluster_name'],
+        gcp_conn_id=cluster_defaults['gcp_conn_id'],
+        arguments=[gs_data_output_bucket.format('contactpersons'),
+                   gs_data_output_bucket.format('contactpersons_merged_1')])
+
+    merge_operators >> merge_contactpersons1
+    globals()['contactpersons_to_parquet'] >> merge_contactpersons1
+
+    merge_contactpersons2 = DataProcSparkOperator(
+        task_id='merge_contactpersons_2',
+        main_class='com.unilever.ohub.spark.merging.ContactPersonMerging2',
+        dataproc_spark_jars=cluster_defaults['jars'],
+        cluster_name=cluster_defaults['cluster_name'],
+        gcp_conn_id=cluster_defaults['gcp_conn_id'],
+        arguments=[gs_data_output_bucket.format('contactpersons_merged_1'),
+                   gs_data_output_bucket.format('operators_merged'),
+                   gs_data_output_bucket.format('contactpersons_merged_2')])
+
+    merge_contactpersons1 >> merge_contactpersons2 >> globals()['contactpersons_to_acm']
+
+    merge_orders = DataProcSparkOperator(
+        task_id='merge_orders',
+        main_class='com.unilever.ohub.spark.merging.OrderMerging',
+        dataproc_spark_jars=cluster_defaults['jars'],
+        cluster_name=cluster_defaults['cluster_name'],
+        gcp_conn_id=cluster_defaults['gcp_conn_id'],
+        arguments=[gs_data_output_bucket.format('contactpersons_merged'),
+                   gs_data_output_bucket.format('operators_merged'),
+                   gs_data_output_bucket.format('contactpersons_merged')])
+
+    merge_operators >> merge_orders >> globals()['orders_to_acm']
+    merge_contactpersons2 >> merge_orders
+    globals()['orders_to_parquet'] >> merge_orders
+
+    globals()['products_to_parquet'] >> globals()['products_to_acm']
