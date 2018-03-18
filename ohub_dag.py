316af57b017f32296e6e1e914ff2e007ac95f937
Tim van Cann <timvancann@godatadriven.com>
2018-03-18 14:16:28 +0100

    ohub dag to databricks

--- ohub_dag.py
+++ ohub_dag.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.contrib.operators.dataproc_operator import DataprocClusterCreateOperator, DataprocClusterDeleteOperator, \
-    DataProcSparkOperator, DataProcPySparkOperator
 
 from config import email_addresses
+from custom_operators.databricks_functions import \
+    DatabricksCreateClusterOperator, \
+    DatabricksDeleteClusterOperator, \
+    DatabricksSubmitRunOperator
 
 default_args = {
     'owner': 'airflow',
@@ -18,20 +20,9 @@ default_args = {
     'pool': 'ohub-pool',
 }
 
-gs_deployment = 'gs://ufs-prod/deployment/'
-gs_jar_bucket = f'{gs_deployment}/ohub2.0'
-gs_py_bucket = f'{gs_deployment}/name-matching'
-gs_data_input_bucket = 'gs://ufs-prod/data/raw/{}/**/*.csv'
-gs_data_output_bucket = 'gs://ufs-prod/data/parquet/{}.parquet'
-jars = [f'{gs_jar_bucket}/spark-jobs-assembly-0.1.jar']
-gs_init_scripts_bucket = 'gs://ufs-prod/jobs/dataproc-init'
-
-cluster_defaults = {
-    'gcp_conn_id': 'airflow-sp',
-    'cluster_name': 'ohub',
-    'project_id': 'ufs-prod',
-    'region': 'global',  # has to be global due to bug in airflow dataproc code
-}
+wasb_root_bucket = 'wasbs://prod@ulohub2storedevne.blob.core.windows.net/data/'
+data_input_bucket = wasb_root_bucket + 'raw/{}/**/*.csv'
+data_output_bucket = wasb_root_bucket + 'parquet/{}.parquet'
 
 csv_to_parquet = [
     {'class': 'OperatorConverter',
@@ -59,91 +50,133 @@ to_acm = [
      'input': 'products'},
 ]
 
-spark_task_defaults = {
-    'dataproc_spark_jars': jars,
-    'cluster_name': cluster_defaults['cluster_name'],
-    'gcp_conn_id': cluster_defaults['gcp_conn_id'],
+cluster_name = 'ohub_basic'
+databricks_conn_id = 'databricks_azure'
+
+cluster_config = {
+    'cluster_name': cluster_name,
+    "spark_version": "3.5.x-scala2.11",
+    "node_type_id": "Standard_DS3_v2",
+    "num_workers": 16
 }
 with DAG('ohub_dag', default_args=default_args,
          schedule_interval="@once") as dag:
-    create_cluster = DataprocClusterCreateOperator(
+    create_cluster = DatabricksCreateClusterOperator(
         task_id='create_cluster',
-        num_workers=8,
-        worker_machine_type='n1-standard-16',
-        zone='europe-west4-c',
-        init_actions_uris=[f'{gs_init_scripts_bucket}/ufs-conda.sh'],
-        **cluster_defaults)
+        cluster_config=cluster_config,
+        databricks_conn_id=databricks_conn_id,
+        polling_period_seconds=10
+    )
 
-    delete_cluster = DataprocClusterDeleteOperator(
-        task_id='delete_cluster',
-        **cluster_defaults)
+    delete_cluster = DatabricksDeleteClusterOperator(
+        task_id='destroy_cluster',
+        cluster_name=cluster_name,
+        databricks_conn_id=databricks_conn_id
+    )
 
     for task in csv_to_parquet:
         task_name = f"{task['input'].lower()}_to_parquet"
-        globals()[task_name] = DataProcSparkOperator(
+        globals()[task_name] = DatabricksSubmitRunOperator(
             task_id=task_name,
-            main_class=f"com.unilever.ohub.spark.tsv2parquet.{task['class']}",
-            **spark_task_defaults,
-            arguments=[gs_data_input_bucket.format(task['input']),
-                       gs_data_output_bucket.format(task['input'].lower())])
+            cluster_name=cluster_name,
+            libraries=[
+                {'jar': 'dbfs:/libraries/spark-jobs-assembly-0.1.jar'}
+            ],
+            spark_jar_task={
+                'main_class_name': f"com.unilever.ohub.spark.tsv2parquet.{task['class']}",
+                'parameters': [data_input_bucket.format(task['input']),
+                               data_output_bucket.format(task['input'].lower())]
+            }
+        )
+
         create_cluster >> globals()[task_name]
 
     for task in to_acm:
         task_name = f"{task['output'].lower()}_to_acm"
-        globals()[task_name] = DataProcSparkOperator(
+        globals()[task_name] = DatabricksSubmitRunOperator(
             task_id=task_name,
-            main_class=f"com.unilever.ohub.spark.acm.{task['class']}",
-            **spark_task_defaults,
-            arguments=[gs_data_input_bucket.format(task['input']),
-                       gs_data_output_bucket.format(task['output']) + '_acm'])
+            cluster_name=cluster_name,
+            libraries=[
+                {'jar': 'dbfs:/libraries/spark-jobs-assembly-0.1.jar'}
+            ],
+            spark_jar_task={
+                'main_class_name': f"com.unilever.ohub.spark.tsv2parquet.{task['class']}",
+                'parameters': [data_input_bucket.format(task['input']),
+                               data_output_bucket.format(task['output']) + '_acm']
+            })
         globals()[task_name] >> delete_cluster
 
-    match_operators = DataProcPySparkOperator(
+    match_operators = DatabricksSubmitRunOperator(
         task_id='match_operators',
-        main=f'{gs_py_bucket}/match_operators.py',
-        pyfiles=[f'{gs_py_bucket}/dist/string_matching.egg'],
-        **spark_task_defaults,
-        arguments=['--input_file', gs_data_output_bucket.format('operators'),
-                   '--output_path', gs_data_output_bucket.format('operators_matched')])
+        cluster_name=cluster_name,
+        libraries=[
+            {'egg': 'dbfs:/libraries/string_matching.egg'}
+        ],
+        spark_python_task={
+            'python_file': 'dbfs:/libraries/match_operators.py',
+            'parameters': ['--input_file', data_output_bucket.format('operators'),
+                           '--output_path', data_output_bucket.format('operators_matched')]
+        }
+    )
 
-    merge_operators = DataProcSparkOperator(
+    merge_operators = DatabricksSubmitRunOperator(
         task_id='merge_operators',
-        main_class='com.unilever.ohub.spark.merging.OperatorMerging',
-        **spark_task_defaults,
-        arguments=[gs_data_output_bucket.format('operators_matched'),
-                   gs_data_input_bucket.format('OPERATORS'),
-                   gs_data_output_bucket.format('operators_merged')])
+        cluster_name=cluster_name,
+        libraries=[
+            {'jar': 'dbfs:/libraries/spark-jobs-assembly-0.1.jar'}
+        ],
+        spark_jar_task={
+            'main_class_name': f"com.unilever.ohub.spark.merging.OperatorMerging",
+            'parameters': [data_output_bucket.format('operators_matched'),
+                           data_input_bucket.format('OPERATORS'),
+                           data_output_bucket.format('operators_merged')]
+        })
 
     operators_to_parquet >> match_operators >> merge_operators >> operators_to_acm
 
-    merge_contactpersons1 = DataProcSparkOperator(
+    merge_contactpersons1 = DatabricksSubmitRunOperator(
         task_id='merge_contactpersons_1',
-        main_class='com.unilever.ohub.spark.merging.ContactPersonMerging1',
-        **spark_task_defaults,
-        arguments=[gs_data_output_bucket.format('contactpersons'),
-                   gs_data_output_bucket.format('contactpersons_merged_1')])
+        cluster_name=cluster_name,
+        libraries=[
+            {'jar': 'dbfs:/libraries/spark-jobs-assembly-0.1.jar'}
+        ],
+        spark_jar_task={
+            'main_class_name': f"com.unilever.ohub.spark.merging.ContactPersonMerging1",
+            'parameters': [data_output_bucket.format('contactpersons'),
+                           data_output_bucket.format('contactpersons_merged_1')]
+        })
 
     merge_operators >> merge_contactpersons1
     contactpersons_to_parquet >> merge_contactpersons1
 
-    merge_contactpersons2 = DataProcSparkOperator(
+    merge_contactpersons2 = DatabricksSubmitRunOperator(
         task_id='merge_contactpersons_2',
-        main_class='com.unilever.ohub.spark.merging.ContactPersonMerging2',
-        **spark_task_defaults,
-        arguments=[gs_data_output_bucket.format('contactpersons_merged_1'),
-                   gs_data_output_bucket.format('operators_merged'),
-                   gs_data_output_bucket.format('contactpersons_merged_2')])
+        cluster_name=cluster_name,
+        libraries=[
+            {'jar': 'dbfs:/libraries/spark-jobs-assembly-0.1.jar'}
+        ],
+        spark_jar_task={
+            'main_class_name': f"com.unilever.ohub.spark.merging.ContactPersonMerging2",
+            'parameters': [data_output_bucket.format('contactpersons_merged_1'),
+                           data_output_bucket.format('operators_merged'),
+                           data_output_bucket.format('contactpersons_merged_2')]
+        })
 
     merge_contactpersons1 >> merge_contactpersons2 >> contactpersons_to_acm
 
-    merge_orders = DataProcSparkOperator(
+    merge_orders = DatabricksSubmitRunOperator(
         task_id='merge_orders',
-        main_class='com.unilever.ohub.spark.merging.OrderMerging',
-        **spark_task_defaults,
-        arguments=[gs_data_output_bucket.format('contactpersons_merged_2'),
-                   gs_data_output_bucket.format('operators_merged'),
-                   gs_data_output_bucket.format('orders'),
-                   gs_data_output_bucket.format('orders_merged')])
+        cluster_name=cluster_name,
+        libraries=[
+            {'jar': 'dbfs:/libraries/spark-jobs-assembly-0.1.jar'}
+        ],
+        spark_jar_task={
+            'main_class_name': f"com.unilever.ohub.spark.merging.OrderMerging",
+            'parameters': [data_output_bucket.format('contactpersons_merged_2'),
+                           data_output_bucket.format('operators_merged'),
+                           data_output_bucket.format('orders'),
+                           data_output_bucket.format('orders_merged')]
+        })
 
     merge_operators >> merge_orders >> globals()['orders_to_acm']
     merge_contactpersons2 >> merge_orders
