c8aee8f57181f0cd423561950b6a04071966a0ee
Gerard van de Glind <gvandeglind@xebia.nl>
2018-03-27 13:48:22 +0200

    OHUB-217: add domain gate keeper for Emakina, File & Fuzzit

---
+++ src/main/scala/com/unilever/ohub/spark/tsv2parquet/emakina/EmakinaDomainGateKeeper.scala
@@ -0,0 +1,10 @@
+package com.unilever.ohub.spark.tsv2parquet.emakina
+
+import com.unilever.ohub.spark.domain.DomainEntity
+import com.unilever.ohub.spark.tsv2parquet.DomainGateKeeper
+
+trait EmakinaDomainGateKeeper[T <: DomainEntity] extends DomainGateKeeper[T] {
+  override final val fieldSeparator = ";"
+  override final val hasHeaders = true
+  override final val partitionByValue = Seq("countryCode")
+}
