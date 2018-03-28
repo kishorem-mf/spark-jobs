05c2e6f79018f7203203d1dab9ad41f17be513ae
Tim van Cann <timvancann@godatadriven.com>
2018-03-28 15:59:24 +0200

    Change ouniverse to run per date

--- ouniverse/new_leads.py
+++ ouniverse/new_leads.py
@@ -6,12 +6,12 @@ from airflow.models import Variable
 from custom_operators.databricks_functions import \
     DatabricksCreateClusterOperator, \
     DatabricksTerminateClusterOperator, \
-    DatabricksSubmitRunOperator
+    DatabricksSubmitRunOperator, DatabricksStartClusterOperator
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 2, 6),
+    'start_date': datetime(2018, 1, 11),
     'email': ['timvancann@godatadriven.com'],
     'email_on_failure': True,
     'email_on_retry': False,
@@ -19,94 +19,89 @@ default_args = {
 }
 
 wasb_root_bucket = 'wasbs://prod@ulohub2storedevne.blob.core.windows.net/data/'
-data_bucket = wasb_root_bucket + 'ouniverse'
+data_input_bucket = wasb_root_bucket + 'raw/ouniverse/'
+data_output_bucket = wasb_root_bucket + 'processed/ouniverse/'
 
-cluster_name = 'ouniverse_new_leads'
+cluster_name = 'ouniverse'
+cluster_id = '0320-064521-hex234'
 databricks_conn_id = 'databricks_azure'
-
-cluster_config = {
-    'cluster_name': cluster_name,
-    "spark_version": "3.5.x-scala2.11",
-    "node_type_id": "Standard_DS3_v2",
-    "num_workers": 16
-}
 
 with DAG('new_leads', default_args=default_args,
          schedule_interval='@once') as dag:
-    create_cluster = DatabricksCreateClusterOperator(
+    create_cluster = DatabricksStartClusterOperator(
         task_id='create_cluster',
-        cluster_config=cluster_config,
+        cluster_id=cluster_id,
         databricks_conn_id=databricks_conn_id,
         polling_period_seconds=10
     )
 
     delete_cluster = DatabricksTerminateClusterOperator(
-        task_id='destroy_cluster',
-        cluster_name=cluster_name,
+        task_id='terminate_cluster',
+        cluster_id=cluster_id,
         databricks_conn_id=databricks_conn_id
     )
 
     phase_one = DatabricksSubmitRunOperator(
         task_id="phase_one",
-        cluster_name=cluster_name,
+        cluster_id=cluster_id,
         databricks_conn_id=databricks_conn_id,
         libraries=[
             {'jar': 'dbfs:/libraries/ouniverse-prioritisation-assembly-1.0.0-SNAPSHOT.jar'}
         ],
         spark_jar_task={
             'main_class_name': "com.unilever.ouniverse.leads.OperatorMapsSearcher",
-            'parameters': ['--operators', '{}/input/phase_I_NZ_sample.csv'.format(data_bucket),
-                           '--outputpath', '{}/output/phaseI_output'.format(data_bucket),
+            'parameters': ['--operators', '{}/operators/{{ds}}/*.csv'.format(data_input_bucket),
+                           '--outputpath', '{}/operators/{{ds}}/operators.parquet'.format(data_output_bucket),
                            '--apiKey', Variable.get('google_api_key')]
         }
     )
 
     phase_two_grid = DatabricksSubmitRunOperator(
         task_id="phase_two_grid",
-        cluster_name=cluster_name,
+        cluster_id=cluster_id,
         databricks_conn_id=databricks_conn_id,
         libraries=[
             {'jar': 'dbfs:/libraries/ouniverse-prioritisation-assembly-1.0.0-SNAPSHOT.jar'}
         ],
         spark_jar_task={
             'main_class_name': "com.unilever.ouniverse.leads.GridSearcher",
-            'parameters': ['--leads', '{}/input/Phase_II_Input_NZ_sample.csv'.format(data_bucket),
-                           '--outputpath', '{}/output/phaseIIa_output'.format(data_bucket),
+            'parameters': ['--leads', '{}/grid/{{ds}}/*.csv'.format(data_input_bucket),
+                           '--outputpath', '{}/grid/{{ds}}/grid.parquet'.format(data_output_bucket),
                            '--apiKey', Variable.get('google_api_key')]
         }
     )
 
     phase_two_ids = DatabricksSubmitRunOperator(
         task_id="phase_two_ids",
-        cluster_name=cluster_name,
+        cluster_id=cluster_id,
         databricks_conn_id=databricks_conn_id,
         libraries=[
             {'jar': 'dbfs:/libraries/ouniverse-prioritisation-assembly-1.0.0-SNAPSHOT.jar'}
         ],
         spark_jar_task={
             'main_class_name': "com.unilever.ouniverse.leads.PlaceIdSearcher",
-            'parameters': ['--places', '{}/output/phaseIIa_output'.format(data_bucket),
+            'parameters': ['--places', '{}/grid/{{ds}}/*'.format(data_output_bucket),
                            '--idColumn', 'placeId',
                            '--fileType', 'parquet',
-                           '--outputpath', '{}/output/phaseIIb_output'.format(data_bucket),
+                           '--outputpath', '{}/place_details/{{ds}}/details.parquet'.format(data_output_bucket),
                            '--apiKey', Variable.get('google_api_key')]
         }
     )
 
     prioritize = DatabricksSubmitRunOperator(
         task_id="prioritise_leads",
-        cluster_name=cluster_name,
+        cluster_id=cluster_id,
         databricks_conn_id=databricks_conn_id,
         libraries=[
             {'jar': 'dbfs:/libraries/ouniverse-prioritisation-assembly-1.0.0-SNAPSHOT.jar'}
         ],
         spark_jar_task={
             'main_class_name': "com.unilever.ouniverse.prioritisation.PrioritizeLeads",
-            'parameters': ['--operators', '{}/output/phaseI_output'.format(data_bucket),
-                           '--places', '{}/output/phaseIIb_output'.format(data_bucket),
-                           '--leads', '{}/input/cities.csv'.format(data_bucket),
-                           '--priorities', '{}/input/priorities.csv'.format(data_bucket),
-                           '--outputpath', '{}/output/phaseIII_output'.format(data_bucket)]
+            'parameters': ['--operators', '{}/operators/{{ds}}/*'.format(data_output_bucket),
+                           '--places', '{}/place_detail/{{ds}}/*'.format(data_output_bucket),
+                           '--leads', '{}/cities.csv'.format(data_output_bucket),
+                           '--priorities', '{}/priorities.csv'.format(data_output_bucket),
+                           '--outputpath', '{}/leads/{{ds}}/leads.parquet'.format(data_output_bucket)]
         }
     )
 
