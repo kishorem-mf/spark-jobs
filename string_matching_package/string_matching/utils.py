1d56f531a8d1f3d6f0449fc99e9ec37033c30390
Tim van Cann <timvancann@godatadriven.com>
2018-03-29 15:08:03 +0200

    Fix None input

--- string_matching_package/string_matching/utils.py
+++ string_matching_package/string_matching/utils.py
@@ -173,6 +173,8 @@ class Timer(object):
 
 
 def remove_strange_chars_to_lower_and_trim(input: str):
+    if input is None:
+        return input
     p = re.compile(
         "(^\\s+)|(\\s+$)|[\u0024\u00A2\u00A3\u00A4\u00A5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0"
         "\u20A1\u20A2\u20A3\u20A4\u20A5\u20A6\u20A7\u20A8\u20A9\u20AA\u20AB\u20AC\u20AD\u20AE\u20AF\u20B0\u20B1\u20B2"
@@ -184,6 +186,8 @@ def remove_strange_chars_to_lower_and_trim(input: str):
 
 
 def remove_spaces_strange_chars_and_to_lower(input: str):
+    if input is None:
+        return input
     p = re.compile(
         "[ \u0024\u00A2\u00A3\u00A4\u00A5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0\u20A1\u20A2"
         "\u20A3\u20A4\u20A5\u20A6\u20A7\u20A8\u20A9\u20AA\u20AB\u20AC\u20AD\u20AE\u20AF\u20B0\u20B1\u20B2\u20B3\u20B4"
