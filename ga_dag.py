4997ad79c7a3d3a43b4e8f68ad05f628c7b30a79
Tim van Cann <timvancann@godatadriven.com>
2018-03-29 18:28:06 +0200

    Run daily at 04.00 for 2 days ago

--- ga_dag.py
+++ ga_dag.py
@@ -12,7 +12,7 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=2),
-    'start_date': datetime(2018, 3, 28),
+    'start_date': datetime(2018, 3, 26),
 }
 
 local_path = '/tmp/gs_export/'
