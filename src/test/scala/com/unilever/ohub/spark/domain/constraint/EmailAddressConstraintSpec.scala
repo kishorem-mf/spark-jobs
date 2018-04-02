77298f77573ee8f63850c00805037829a1eb67b4
Gerard van de Glind <gvandeglind@xebia.nl>
2018-04-02 21:31:42 +0200

    OHUB-217: add email address constraint spec

---
+++ src/test/scala/com/unilever/ohub/spark/domain/constraint/EmailAddressConstraintSpec.scala
@@ -0,0 +1,30 @@
+package com.unilever.ohub.spark.domain.constraint
+
+import com.unilever.ohub.spark.domain.DomainConstraintViolationException
+import org.scalatest.{Matchers, WordSpec}
+
+class EmailAddressConstraintSpec extends WordSpec with Matchers {
+
+  "Email address constraint" should {
+    "throw a DomainConstraintViolationException" when {
+      "an invalid email address is provided" in {
+        intercept[DomainConstraintViolationException] {
+          EmailAddressConstraint.validate("some-string-value")
+        }
+        intercept[DomainConstraintViolationException] {
+          EmailAddressConstraint.validate("some-string-value@")
+        }
+        intercept[DomainConstraintViolationException] {
+          EmailAddressConstraint.validate("some-string-value@something")
+        }
+      }
+    }
+
+    "not throw a DomainConstraintViolationException" when {
+      "a valid email address is provided" in {
+        EmailAddressConstraint.validate("my-email-address@some-server.com")
+        EmailAddressConstraint.validate("my.extra.long.email.address@some.long.distance.server.com")
+      }
+    }
+  }
+}
