6b9264085031ba667a0a1ffecc0797439119ebbd
Tim van Cann <timvancann@godatadriven.com>
2018-04-04 14:50:29 +0200

    Add more tests

--- src/test/scala/com/unilever/ohub/spark/merging/OperatorUpdateGoldenRecordSpec.scala
+++ src/test/scala/com/unilever/ohub/spark/merging/OperatorUpdateGoldenRecordSpec.scala
@@ -1,10 +1,13 @@
 package com.unilever.ohub.spark.merging
 
 import com.unilever.ohub.spark.SparkJobSpec
+import com.unilever.ohub.spark.SharedSparkSession.spark
 
 class OperatorUpdateGoldenRecordSpec extends SparkJobSpec {
 
-  describe("picking golden record") {
+  import spark.implicits._
+
+  describe("marking golden record") {
     it("should pick the operator with highest sourcePreference") {
       val sourcePreferences = Map(
         "sourceA" -> 2,
@@ -35,6 +38,43 @@ class OperatorUpdateGoldenRecordSpec extends SparkJobSpec {
       assert(golden.size === 2)
       assert(!golden.find(_.sourceName == "sourceA").get.isGoldenRecord)
       assert(golden.find(_.sourceName == "sourceB").get.isGoldenRecord)
+    }
+  }
+
+  describe("updating golden record") {
+    it("should not change anything if not needed") {
+      val sourcePreferences = Map(
+        "sourceA" -> 2,
+        "sourceB" -> 1
+      )
+
+      val a = defaultOperatorRecord.copy(sourceName = "sourceA")
+      val b = defaultOperatorRecord.copy(sourceName = "sourceB", isGoldenRecord = true)
+
+      val operators = Seq(a,b).toDataset
+
+      val updated = OperatorUpdateGoldenRecord.transform(spark, operators, sourcePreferences).collect
+
+      assert(updated.find(_.sourceName == "sourceA").get === a)
+      assert(updated.find(_.sourceName == "sourceB").get === b)
+
+    }
+    it("should not change anything if needed") {
+      val sourcePreferences = Map(
+        "sourceA" -> 1,
+        "sourceB" -> 2
+      )
+
+      val a = defaultOperatorRecord.copy(sourceName = "sourceA")
+      val b = defaultOperatorRecord.copy(sourceName = "sourceB", isGoldenRecord = true)
+
+      val operators = Seq(a,b).toDataset
+
+      val updated = OperatorUpdateGoldenRecord.transform(spark, operators, sourcePreferences).collect
+
+      assert(updated.find(_.sourceName == "sourceA").get.isGoldenRecord)
+      assert(!updated.find(_.sourceName == "sourceB").get.isGoldenRecord)
+
     }
   }
 }
