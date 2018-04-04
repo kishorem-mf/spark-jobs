35b8e222222827e0982417839c4898df9db8b1b7
Tim van Cann <timvancann@godatadriven.com>
2018-04-04 09:39:17 +0200

    Update and add unit tests

--- src/test/scala/com/unilever/ohub/spark/SparkJobSpec.scala
+++ src/test/scala/com/unilever/ohub/spark/SparkJobSpec.scala
@@ -1,12 +1,18 @@
 package com.unilever.ohub.spark
 
-import org.apache.spark.sql.{ Dataset, Encoder }
-import org.scalatest.{ FunSpec, Matchers }
+import org.apache.spark.sql.{Dataset, Encoder}
+import org.scalatest.{FunSpec, Matchers}
 import SharedSparkSession.spark
 import org.scalamock.scalatest.MockFactory
 
 trait SparkJobSpec extends FunSpec with Matchers with MockFactory {
+
   protected implicit class ObjOps[T: Encoder](obj: T) {
     def toDataset: Dataset[T] = spark.createDataset(Seq(obj))
   }
+
+  protected implicit class ObjsOps[T: Encoder](objs: Seq[T]) {
+    def toDataset: Dataset[T] = spark.createDataset(objs)
+  }
+
 }
--- src/test/scala/com/unilever/ohub/spark/deduplicate/OperatorDeduplicationTest.scala
+++ src/test/scala/com/unilever/ohub/spark/deduplicate/OperatorDeduplicationTest.scala
@@ -3,109 +3,157 @@ package com.unilever.ohub.spark.deduplicate
 import java.sql.Timestamp
 import java.util.UUID
 
+import com.unilever.ohub.spark.SparkJobSpec
 import com.unilever.ohub.spark.domain.entity.Operator
-import org.scalatest.FlatSpec
 import com.unilever.ohub.spark.SharedSparkSession.spark
 
-class OperatorDeduplicationTest extends FlatSpec {
+class OperatorDeduplicationTest extends SparkJobSpec {
 
   import spark.implicits._
 
-  private def generateOperator(concatId:String = UUID.randomUUID().toString,
-                               dateUpdated: Timestamp = new Timestamp(System.currentTimeMillis())
-                              ): Operator = {
+  private val defaultOperatorRecord: Operator =
     Operator(
-      concatId                    = concatId,
+      concatId                    = UUID.randomUUID().toString,
       countryCode                 = "country-code",
       isActive                    = true,
       isGoldenRecord              = false,
-      groupId                     = Some(UUID.randomUUID().toString),
+      groupId                     = None,
       name                        = "operator-name",
       sourceEntityId              = "source-entity-id",
       sourceName                  = "source-name",
       ohubCreated                 = new Timestamp(System.currentTimeMillis()),
       ohubUpdated                 = new Timestamp(System.currentTimeMillis()),
-      averagePrice                = Some(BigDecimal(12345)),
-      chainId                     = Some("chain-id"),
-      chainName                   = Some("chain-name"),
-      channel                     = Some("channel"),
-      city                        = Some("city"),
-      cookingConvenienceLevel     = Some("cooking-convenience-level"),
-      countryName                 = Some("country-name"),
-      customerType                = Some("customer-type"),
-      dateCreated                 = Some(new Timestamp(System.currentTimeMillis())),
-      dateUpdated                 = Some(dateUpdated),
-      daysOpen                    = Some(4),
-      distributorCustomerNumber   = Some("distributor-customer-number"),
-      distributorName             = Some("distributor-name"),
+      averagePrice                = None,
+      chainId                     = None,
+      chainName                   = None,
+      channel                     = None,
+      city                        = None,
+      cookingConvenienceLevel     = None,
+      countryName                 = None,
+      customerType                = None,
+      dateCreated                 = None,
+      dateUpdated                 = Some(new Timestamp(System.currentTimeMillis())),
+      daysOpen                    = None,
+      distributorCustomerNumber   = None,
+      distributorName             = None,
       distributorOperatorId       = None,
-      emailAddress                = Some("email-address@some-server.com"),
-      faxNumber                   = Some("+31123456789"),
+      emailAddress                = None,
+      faxNumber                   = None,
       germanChainId               = None,
       germanChainName             = None,
-      hasDirectMailOptIn          = Some(true),
-      hasDirectMailOptOut         = Some(false),
-      hasEmailOptIn               = Some(true),
-      hasEmailOptOut              = Some(false),
-      hasFaxOptIn                 = Some(true),
-      hasFaxOptOut                = Some(false),
-      hasGeneralOptOut            = Some(false),
-      hasMobileOptIn              = Some(true),
-      hasMobileOptOut             = Some(false),
-      hasTelemarketingOptIn       = Some(true),
-      hasTelemarketingOptOut      = Some(false),
-      houseNumber                 = Some("12"),
+      hasDirectMailOptIn          = None,
+      hasDirectMailOptOut         = None,
+      hasEmailOptIn               = None,
+      hasEmailOptOut              = None,
+      hasFaxOptIn                 = None,
+      hasFaxOptOut                = None,
+      hasGeneralOptOut            = None,
+      hasMobileOptIn              = None,
+      hasMobileOptOut             = None,
+      hasTelemarketingOptIn       = None,
+      hasTelemarketingOptOut      = None,
+      houseNumber                 = None,
       houseNumberExtension        = None,
-      isNotRecalculatingOtm       = Some(true),
-      isOpenOnFriday              = Some(true),
-      isOpenOnMonday              = Some(false),
-      isOpenOnSaturday            = Some(true),
-      isOpenOnSunday              = Some(false),
-      isOpenOnThursday            = Some(true),
-      isOpenOnTuesday             = Some(true),
-      isOpenOnWednesday           = Some(true),
-      isPrivateHousehold          = Some(false),
-      kitchenType                 = Some("kitchen-type"),
-      mobileNumber                = Some("+31612345678"),
-      netPromoterScore            = Some(BigDecimal(75)),
-      oldIntegrationId            = Some("old-integration-id"),
-      otm                         = Some("otm"),
-      otmEnteredBy                = Some("otm-entered-by"),
-      phoneNumber                 = Some("+31123456789"),
-      region                      = Some("region"),
-      salesRepresentative         = Some("sales-representative"),
-      state                       = Some("state"),
-      street                      = Some("street"),
-      subChannel                  = Some("sub-channel"),
-      totalDishes                 = Some(150),
-      totalLocations              = Some(25),
-      totalStaff                  = Some(105),
-      vat                         = Some("vat"),
-      webUpdaterId                = Some("web-updater-id"),
-      weeksClosed                 = Some(2),
-      zipCode                     = Some("1234 AB"),
+      isNotRecalculatingOtm       = None,
+      isOpenOnFriday              = None,
+      isOpenOnMonday              = None,
+      isOpenOnSaturday            = None,
+      isOpenOnSunday              = None,
+      isOpenOnThursday            = None,
+      isOpenOnTuesday             = None,
+      isOpenOnWednesday           = None,
+      isPrivateHousehold          = None,
+      kitchenType                 = None,
+      mobileNumber                = None,
+      netPromoterScore            = None,
+      oldIntegrationId            = None,
+      otm                         = None,
+      otmEnteredBy                = None,
+      phoneNumber                 = None,
+      region                      = None,
+      salesRepresentative         = None,
+      state                       = None,
+      street                      = None,
+      subChannel                  = None,
+      totalDishes                 = None,
+      totalLocations              = None,
+      totalStaff                  = None,
+      vat                         = None,
+      webUpdaterId                = None,
+      weeksClosed                 = None,
+      zipCode                     = None,
       ingestionErrors             = Map()
     )
-  }
-  behavior of "deduplication"
 
-  it should "find no duplicates if none are there" in {
-    val integrated = spark.createDataset(Seq(
-      generateOperator("a"),
-      generateOperator("b")
-    ))
-    val daily = spark.createDataset(Seq(
-      generateOperator("c")
-    ))
+  describe("deduplication") {
 
-    val (updatedDs, dailyNewDs) = OperatorDeduplication.transform(spark, integrated, daily)
+    it("should return integrated if daily is empty") {
+      val integrated = defaultOperatorRecord.toDataset
 
-    val updated = updatedDs.collect
-    val dailyNew = dailyNewDs.collect
-    assert(updated.length === 2)
-    assert(dailyNew.length === 1)
+      val daily = spark.emptyDataset[Operator]
 
-    assert(updated.map(_.concatId) === Seq("a", "b"))
-    assert(dailyNew.map(_.concatId) === Seq("c"))
+      val (updatedDs, dailyNewDs) = OperatorDeduplication.transform(spark, integrated, daily)
+
+      val updated = updatedDs.collect
+      val dailyNew = dailyNewDs.collect
+      assert(updated.length === 1)
+      assert(dailyNew.length === 0)
+    }
+
+    it("should find no duplicates if none are there") {
+      val integrated = Seq(
+        defaultOperatorRecord.copy(concatId = "a"),
+        defaultOperatorRecord.copy(concatId = "b")
+      ).toDataset
+
+      val daily = defaultOperatorRecord.copy(concatId = "c").toDataset
+
+      val (updatedDs, dailyNewDs) = OperatorDeduplication.transform(spark, integrated, daily)
+
+      val updated = updatedDs.collect
+      val dailyNew = dailyNewDs.collect
+      assert(updated.length === 2)
+      assert(dailyNew.length === 1)
+
+      assert(updated.sortBy(_.concatId).map(_.concatId) === Seq("a", "b"))
+      assert(dailyNew.head.concatId === "c")
+    }
+
+    it("should return a deduplicated dataset if there are duplicates") {
+      val integrated = Seq(
+        defaultOperatorRecord.copy(concatId = "a"),
+        defaultOperatorRecord.copy(concatId = "b")
+      ).toDataset
+      val daily = defaultOperatorRecord.copy(concatId = "a").toDataset
+
+      val (updatedDs, dailyNewDs) = OperatorDeduplication.transform(spark, integrated, daily)
+
+      val updated = updatedDs.collect
+      val dailyNew = dailyNewDs.collect
+      assert(updated.length === 2)
+      assert(dailyNew.length === 0)
+
+      assert(updated.map(_.concatId) === Seq("a", "b"))
+    }
+
+    it("return a deduplicated dataset with the newest record if there are duplicates") {
+      val integrated = Seq(
+        defaultOperatorRecord.copy(concatId = "a", dateUpdated = Some(Timestamp.valueOf("2017-05-25 12:00:00"))),
+        defaultOperatorRecord.copy(concatId = "b")
+      ).toDataset
+
+      val newTimestamp = Some(Timestamp.valueOf("2017-06-25 12:00:00"))
+      val daily = defaultOperatorRecord.copy(concatId = "a", dateUpdated = newTimestamp).toDataset
+
+      val (updatedDs, dailyNewDs) = OperatorDeduplication.transform(spark, integrated, daily)
+
+      val updated = updatedDs.collect
+      val dailyNew = dailyNewDs.collect
+      assert(updated.length === 2)
+      assert(dailyNew.length === 0)
+
+      assert(updated.map(_.concatId) === Seq("a", "b"))
+      assert(updated.find(_.concatId == "a").get.dateUpdated === newTimestamp))
+    }
   }
 }
