287e88a1195bd90b03d43b5d74c4947fc4299810
Tim van Cann <timvancann@godatadriven.com>
2018-04-19 15:37:12 +0200

    Update tests and coverage

--- string_matching_package/tests/test_utils.py
+++ string_matching_package/tests/test_utils.py
@@ -55,7 +55,7 @@ class TestCleaning(object):
     def setup_class(cls):
         cls.data = [('2', 'NL', 'Dave Mustaine ', 'Amsterdam  ', '@barAvenue', '\uFE3614b', '5312BE')]
 
-    def test_match_string_should_be_concat_from_fields(self, spark):
+    def test_cleaning_should_remove_strange_chars(self, spark):
         ddf = spark.createDataFrame(self.data).toDF('id', 'countryCode', 'name', 'city', 'street', 'houseNumber',
                                                     'zipCode')
 
@@ -67,7 +67,7 @@ class TestCleaning(object):
 
         assert res[0][0] == 'dave mustaine'
         assert res[0][1] == 'amsterdam'
-        assert res[0][2] == 'baravenue14b'
+        assert res[0][2] == 'baravenue\uFE3614b'
         assert res[0][3] == '5312be'
 
 
@@ -75,7 +75,7 @@ class TestMatchingString(object):
 
     @classmethod
     def setup_class(cls):
-        cls.data = [('dave mustaine ', 'amsterdam  ', 'barAvenue14b', '5312be')]
+        cls.data = [('dave mustaine ', 'amsterdam  ', 'barAvenue\uFE3614b', '5312be')]
 
     def test_match_string_should_be_concat_from_fields(self, spark):
         ddf = spark.createDataFrame(self.data).toDF('nameCleansed', 'cityCleansed', 'streetCleansed', 'zipCodeCleansed')
