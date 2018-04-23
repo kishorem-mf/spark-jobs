01707d969f78605dd47248c5bfaedaa5dd30fe4e
Tim van Cann <timvancann@godatadriven.com>
2018-04-23 13:39:45 +0200

    Add slack on failure messag

--- dags/fail_dag.py
+++ dags/fail_dag.py
@@ -2,16 +2,16 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from config import email_addresses
+from config import email_addresses, on_failure_callback
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2018, 2, 6),
     'email': email_addresses,
-    'email_on_failure': True,
-    'email_on_retry': True,
-    'retries': 1,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
     'retry_delay': timedelta(seconds=30)
 }
 
@@ -19,4 +19,5 @@ with DAG('fail_test', default_args=default_args,
          schedule_interval="@once") as dag:
     create_cluster = BashOperator(
         task_id='fail_me',
-        bash_command='return 1')
+        bash_command='return 1',
+        on_failure_callback=on_failure_callback()())
