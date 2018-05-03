5139e9b7e70bbb981699968b3739651dfbcae749
Tim van Cann <timvancann@godatadriven.com>
2018-05-03 09:03:04 +0200

    OHUB-613: Push down pool to only name matching

--- dags/operators_config.py
+++ dags/operators_config.py
@@ -16,7 +16,6 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'pool': 'ohub_pool',
     'on_failure_callback': slack_on_databricks_failure_callback
 }
 
