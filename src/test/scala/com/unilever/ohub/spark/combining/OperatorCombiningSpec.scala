2700630ad506c42cb43b58d50bc668d0947ecaaf
Tim van Cann <timvancann@godatadriven.com>
2018-05-06 10:57:24 +0200

    OHUB-698: Implement taking the latest version of concatIds

--- src/test/scala/com/unilever/ohub/spark/combining/OperatorCombiningSpec.scala
+++ src/test/scala/com/unilever/ohub/spark/combining/OperatorCombiningSpec.scala
@@ -5,6 +5,7 @@ import com.unilever.ohub.spark.SharedSparkSession.spark
 import com.unilever.ohub.spark.domain.entity.{ Operator, TestOperators }
 
 class OperatorCombiningSpec extends SparkJobSpec with TestOperators {
+
   import spark.implicits._
 
   describe("combining") {
@@ -28,6 +29,22 @@ class OperatorCombiningSpec extends SparkJobSpec with TestOperators {
 
       assert(res.length === 3)
       res.map(_.sourceEntityId) should contain theSameElementsAs Seq("source-entity-id-a", "source-entity-id-b", "source-entity-id-c")
+    }
+    it("should combine two operator datasets and take the latest concatId") {
+      val d1 = Seq(
+        defaultOperatorWithSourceEntityId("source-entity-id-a"),
+        defaultOperatorWithSourceEntityId("source-entity-id-b").copy(name = "foo")
+      ).toDataset
+      val d2 = Seq(
+        defaultOperatorWithSourceEntityId("source-entity-id-c"),
+        defaultOperatorWithSourceEntityId("source-entity-id-b").copy(name = "bar")
+      ).toDataset
+
+      val res = OperatorCombining.transform(spark, d1, d2).collect()
+
+      assert(res.length === 3)
+      res.map(_.sourceEntityId) should contain theSameElementsAs Seq("source-entity-id-a", "source-entity-id-b", "source-entity-id-c")
+      res.find(_.sourceEntityId == "source-entity-id-b").get.name shouldBe "bar"
     }
   }
 }
