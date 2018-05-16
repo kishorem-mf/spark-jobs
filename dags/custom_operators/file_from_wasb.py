57089173467f896941478b970d5c21f7199eb04b
Tycho Grouwstra <Tycho.Grouwstra@unilever.com>
2018-05-16 09:52:34 +0200

    new wasb hook

--- dags/custom_operators/file_from_wasb.py
+++ dags/custom_operators/file_from_wasb.py
@@ -1,7 +1,8 @@
 # from: https://github.com/apache/incubator-airflow/blob/master/
 # airflow/contrib/operators/file_to_wasb.py
 
-from airflow.contrib.hooks.wasb_hook import WasbHook
+# from airflow.contrib.hooks.wasb_hook import WasbHook
+from wasb_hook import WasbHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
