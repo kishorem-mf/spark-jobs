e3bb5c9ee6f9e3e0f13b6badfbf1bb12962de065
Tycho Grouwstra <Tycho.Grouwstra@unilever.com>
2018-05-17 15:36:02 +0200

    debug-verify

--- dags/custom_operators/sftp_verify_operator.py
+++ dags/custom_operators/sftp_verify_operator.py
@@ -20,7 +20,6 @@ from airflow.contrib.hooks.ssh_hook import SSHHook
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
-import errno
 
 
 class SftpVerifyOperator(BaseOperator):
