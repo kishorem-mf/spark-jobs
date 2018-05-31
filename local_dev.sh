87d3278af30c93e3a000f8289fce9e45c5617e09
Tim van Cann <timvancann@godatadriven.com>
2018-05-31 11:01:52 +0200

    Revert "Merged in ci-dags (pull request #81)"
    This reverts commit 6e6f1521488a26b8ba7b3d03da59dee13b189d0b, reversing
    changes made to c5957f52032d61488f16d3f51d22b955cf24c959.

--- local_dev.sh
+++ local_dev.sh
@@ -1,18 +1,42 @@
 #!/usr/bin/env bash
-container="ulohub_airflow_dags"
-latest_tag="`az acr repository show-tags --name ulohubimages --repository airflow --output tsv | sort -gr | head -n 1`" 
-docker stop ${container}
-docker rm ${container}
+
+container_name="ulohub_airflow_dags"
+
+printf "\n----------- stopping possibly running container --------------\n"
+docker stop ${container_name}
+printf "\n-----------       removing old container        --------------\n"
+docker rm ${container_name}
+
+printf "\n-----------      updating image if needed       --------------\n"
+latest_tag="`az acr repository show-tags --name ulohubimages --repository airflow --output tsv | sort -gr | head -n 1`"
+docker pull ulohubimages.azurecr.io/airflow:${latest_tag}
+
+printf "\n-----------      starting airflow at 8070       --------------\n"
 docker run \
---name ${container} \
---entrypoint "" \
---mount type=bind,source="`pwd`",target="/usr/local/airflow" \
---mount type=bind,source="`pwd`/dags",target="/usr/local/airflow/dags" \
---env AIRFLOW__CORE__SQL_ALCHEMY_CONN="sqlite:////root/airflow/airflow.db" \
 --env AIRFLOW_HOME="/root/airflow" \
+--env AIRFLOW__CORE__SQL_ALCHEMY_CONN="sqlite:////root/airflow/airflow.db" \
+--env AIRFLOW__CORE__SQL_ALCHEMY_POOL_SIZE="15" \
 --env AIRFLOW__CORE__LOAD_EXAMPLES="False" \
 --env AIRFLOW__CORE__EXECUTOR="SequentialExecutor" \
---env AIRFLOW__CORE__DAGS_FOLDER="/usr/local/airflow/dags" \
---env AIRFLOW__CORE__BASE_LOG_FOLDER="/usr/local/airflow/logs" \
-ulohubimages.azurecr.io/airflow:${latest_tag} \
-bash /usr/local/airflow/test.sh
+--env AIRFLOW__CORE__DAGS_FOLDER="/root/airflow/dags" \
+--env AIRFLOW__CORE__BASE_LOG_FOLDER="/root/airflow/logs" \
+--mount type=bind,source="`pwd`/dags",target="/root/airflow/dags" \
+-p 8070:8080 \
+-d \
+--name ${container_name} \
+ulohubimages.azurecr.io/airflow:61 \
+upgradedb_webserver
+
+sleep 5
+
+printf "\n--------- setting internal airflow configuration  ------------\n"
+docker exec -i ${container_name} airflow pool --set "ohub_pool" "5" ""
+docker exec -i ${container_name} airflow variables --set "google_api_key" "foo"
+docker exec -i ${container_name} airflow connections --add --conn_id "postgres_channels" --conn_uri "foo"
+docker exec -i ${container_name} airflow variables --set "slack_airflow_token" "foo"
+
+sleep 1
+string=`docker exec -i ulohub_airflow_dags airflow list_dags`
+echo "$string"
+substring="Error"
+exit `! [[ $string =~ $substring ]]`
