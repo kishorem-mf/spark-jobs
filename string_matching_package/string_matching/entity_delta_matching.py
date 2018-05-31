16aae8cd99bdc11e2ca18fdec87b330ac8375775
Tim van Cann <timvancann@godatadriven.com>
2018-05-31 17:56:58 +0200

    Drop duplicate ohubId column

--- string_matching_package/string_matching/entity_delta_matching.py
+++ string_matching_package/string_matching/entity_delta_matching.py
@@ -130,7 +130,8 @@ def recreate_matched_and_unmatched(integrated: DataFrame,
     matched_ingested_daily_full_record = (matched
                                           .select('concatId', 'ohubId_matched')
                                           .join(ingested, on='concatId', how='left')
-                                          .withColumnRenamed('ohubId_matched', 'ohubId')
+                                          .withColumn('ohubId', sf.col('ohubId_matched'))
+                                          .drop('ohubId_matched')
                                           )
 
     updated_integrated = (integrated
