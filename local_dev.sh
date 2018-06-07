2cac9d67105a7ead66f6a3d39a00d5efd479bd65
Unknown <unknown@example.com>
2018-06-07 10:47:20 +0200

    make local test WSL-proof

--- local_dev.sh
+++ local_dev.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-if [ "$#" -eq 0 ]; then
-    echo 'Please enter password ACR password.'
-    exit 1
-fi
+[[ "$#" -eq 0 ]] && echo 'Please enter password ACR password.' && exit 1
 container="ulohub_airflow_dags"
+[[ $PWD =~ ^/mnt/$ ]] && path=$PWD || path=${PWD:4:${#PWD}}
 printf "\n----------- stopping possibly running container --------------\n"
 docker stop $container > /dev/null
 printf "\n-----------       removing old container        --------------\n"
@@ -11,16 +9,4 @@ docker rm $container > /dev/null
 img='ulohubimages.azurecr.io/airflow:latest'
 docker pull $img
 docker login ulohubimages.azurecr.io -u ulohubimages -p $1
-docker run \
---env AIRFLOW_HOME="/root/airflow" \
---env AIRFLOW__CORE__SQL_ALCHEMY_CONN="sqlite:////root/airflow/airflow.db" \
---env AIRFLOW__CORE__SQL_ALCHEMY_POOL_SIZE="15" \
---env AIRFLOW__CORE__LOAD_EXAMPLES="False" \
---env AIRFLOW__CORE__EXECUTOR="SequentialExecutor" \
---env AIRFLOW__CORE__DAGS_FOLDER="/usr/app/dags" \
---env AIRFLOW__CORE__BASE_LOG_FOLDER="/usr/app/logs" \
--v $PWD:/usr/app \
--p 8070:8080 \
---entrypoint '' \
-$img \
-bash -c 'bash /usr/app/test.sh && airflow webserver'
+docker run --env AIRFLOW_HOME="/root/airflow" --env AIRFLOW__CORE__SQL_ALCHEMY_CONN="sqlite:////root/airflow/airflow.db" --env AIRFLOW__CORE__SQL_ALCHEMY_POOL_SIZE="15" --env AIRFLOW__CORE__LOAD_EXAMPLES="False" --env AIRFLOW__CORE__EXECUTOR="SequentialExecutor" --env AIRFLOW__CORE__DAGS_FOLDER="/usr/app/dags" --env AIRFLOW__CORE__BASE_LOG_FOLDER="/usr/app/logs" -v $path:/usr/app:ro -p 8070:8080 --entrypoint "" $img bash -c 'bash /usr/app/test.sh && airflow webserver'
