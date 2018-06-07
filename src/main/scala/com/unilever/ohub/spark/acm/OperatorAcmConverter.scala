89fa3fc739bb4dc40d346238a547374c0b23abdb
Tim van Cann <timvancann@godatadriven.com>
2018-06-07 13:56:23 +0200

    OHUB-611: Implement ACM convertor for Product

--- src/main/scala/com/unilever/ohub/spark/acm/OperatorAcmConverter.scala
+++ src/main/scala/com/unilever/ohub/spark/acm/OperatorAcmConverter.scala
@@ -1,6 +1,6 @@
 package com.unilever.ohub.spark.acm
 
-import com.unilever.ohub.spark.{ SparkJob, SparkJobConfig }
+import com.unilever.ohub.spark.{SparkJob, SparkJobConfig}
 import com.unilever.ohub.spark.acm.model.UFSOperator
 import com.unilever.ohub.spark.data.ChannelMapping
 import com.unilever.ohub.spark.domain.entity.Operator
@@ -8,28 +8,19 @@ import com.unilever.ohub.spark.storage.Storage
 import com.unilever.ohub.spark.tsv2parquet.DomainDataProvider
 import org.apache.log4j.Logger
 import com.unilever.ohub.spark.sql.JoinType
-import org.apache.spark.sql.{ Dataset, SparkSession }
+import org.apache.spark.sql.{Dataset, SparkSession}
 import scopt.OptionParser
 
-case class DefaultWithDbAndDeltaConfig(
-    inputFile: String = "path-to-input-file",
-    outputFile: String = "path-to-output-file",
-    previousIntegrated: Option[String] = None,
-    postgressUrl: String = "postgress-url",
-    postgressUsername: String = "postgress-username",
-    postgressPassword: String = "postgress-password",
-    postgressDB: String = "postgress-db"
-) extends SparkJobConfig
 
 object OperatorAcmConverter extends SparkJob[DefaultWithDbAndDeltaConfig]
   with DeltaFunctions with AcmTransformationFunctions with AcmConverter {
 
   def transform(
-    spark: SparkSession,
-    channelMappings: Dataset[ChannelMapping],
-    operators: Dataset[Operator],
-    previousIntegrated: Dataset[Operator]
-  ): Dataset[UFSOperator] = {
+                 spark: SparkSession,
+                 channelMappings: Dataset[ChannelMapping],
+                 operators: Dataset[Operator],
+                 previousIntegrated: Dataset[Operator]
+               ): Dataset[UFSOperator] = {
     val dailyUfsOperators = createUfsOperators(spark, operators, channelMappings)
     val allPreviousUfsOperators = createUfsOperators(spark, previousIntegrated, channelMappings)
 
@@ -38,34 +29,7 @@ object OperatorAcmConverter extends SparkJob[DefaultWithDbAndDeltaConfig]
 
   override private[spark] def defaultConfig = DefaultWithDbAndDeltaConfig()
 
-  override private[spark] def configParser(): OptionParser[DefaultWithDbAndDeltaConfig] =
-    new scopt.OptionParser[DefaultWithDbAndDeltaConfig]("Operator ACM converter") {
-      head("converts domain operators into ufs operators.", "1.0")
-      opt[String]("inputFile") required () action { (x, c) ⇒
-        c.copy(inputFile = x)
-      } text "inputFile is a string property"
-      opt[String]("previousIntegrated") optional () action { (x, c) ⇒
-        c.copy(previousIntegrated = Option(x))
-      } text "previousIntegrated is a string property"
-      opt[String]("outputFile") required () action { (x, c) ⇒
-        c.copy(outputFile = x)
-      } text "outputFile is a string property"
-      opt[String]("postgressUrl") required () action { (x, c) ⇒
-        c.copy(postgressUrl = x)
-      } text "postgressUrl is a string property"
-      opt[String]("postgressUsername") required () action { (x, c) ⇒
-        c.copy(postgressUsername = x)
-      } text "postgressUsername is a string property"
-      opt[String]("postgressPassword") required () action { (x, c) ⇒
-        c.copy(postgressPassword = x)
-      } text "postgressPassword is a string property"
-      opt[String]("postgressDB") required () action { (x, c) ⇒
-        c.copy(postgressDB = x)
-      } text "postgressDB is a string property"
-
-      version("1.0")
-      help("help") text "help text"
-    }
+  override private[spark] def configParser(): OptionParser[DefaultWithDbAndDeltaConfig] = DefaultWithDbAndDeltaConfigParser()
 
   override def run(spark: SparkSession, config: DefaultWithDbAndDeltaConfig, storage: Storage): Unit = {
     import spark.implicits._
@@ -84,7 +48,7 @@ object OperatorAcmConverter extends SparkJob[DefaultWithDbAndDeltaConfig]
     }
     val transformed = transform(spark, channelMappings, operators, previousIntegrated)
 
-    writeToCsv(storage, transformed, config.outputFile)
+    storage.writeToSingleCsv(transformed, config.outputFile, delim = outputCsvDelimiter, quote = outputCsvQuote)
   }
 
   def createUfsOperators(spark: SparkSession, operators: Dataset[Operator], channelMappings: Dataset[ChannelMapping]): Dataset[UFSOperator] = {
@@ -165,9 +129,5 @@ object OperatorAcmConverter extends SparkJob[DefaultWithDbAndDeltaConfig]
           )
         }
       }
-  }
-
-  def writeToCsv(storage: Storage, ds: Dataset[UFSOperator], fileName: String)(implicit log: Logger): Unit = {
-    storage.writeToSingleCsv(ds, fileName, delim = outputCsvDelimiter, quote = outputCsvQuote)
   }
 }
--- src/main/scala/com/unilever/ohub/spark/acm/ProductAcmConverter.scala
+++ src/main/scala/com/unilever/ohub/spark/acm/ProductAcmConverter.scala
@@ -1,14 +1,49 @@
 package com.unilever.ohub.spark.acm
 
-import com.unilever.ohub.spark.{ DefaultConfig, SparkJobWithDefaultConfig }
+import com.unilever.ohub.spark.SparkJob
 import com.unilever.ohub.spark.acm.model.UFSProduct
 import com.unilever.ohub.spark.domain.entity.Product
 import com.unilever.ohub.spark.storage.Storage
-import org.apache.spark.sql.{ Dataset, SparkSession }
+import org.apache.spark.sql.{Dataset, SparkSession}
+import scopt.OptionParser
 
-object ProductAcmConverter extends SparkJobWithDefaultConfig with AcmTransformationFunctions with AcmConverter {
+object ProductAcmConverter extends SparkJob[DefaultWithDbAndDeltaConfig]
+  with DeltaFunctions with AcmTransformationFunctions with AcmConverter {
 
-  def transform(spark: SparkSession, products: Dataset[Product]): Dataset[UFSProduct] = {
+  def transform(
+                 spark: SparkSession,
+                 operators: Dataset[Product],
+                 previousIntegrated: Dataset[Product]
+               ): Dataset[UFSProduct] = {
+    val dailyUfsProducts = createUfsProducts(spark, operators)
+    val allPreviousUfsProducts = createUfsProducts(spark, previousIntegrated)
+
+    integrate[UFSProduct](spark, dailyUfsProducts, allPreviousUfsProducts, "PRD_INTEGRATION_ID")
+  }
+
+  override private[spark] def defaultConfig = DefaultWithDbAndDeltaConfig()
+
+  override private[spark] def configParser(): OptionParser[DefaultWithDbAndDeltaConfig] = DefaultWithDbAndDeltaConfigParser()
+
+  override def run(spark: SparkSession, config: DefaultWithDbAndDeltaConfig, storage: Storage): Unit = {
+    import spark.implicits._
+
+    log.info(s"Generating products ACM csv file from [${config.inputFile}] to [${config.outputFile}]")
+
+    val products = storage.readFromParquet[Product](config.inputFile)
+    val previousIntegrated = config.previousIntegrated match {
+      case Some(s) ⇒ storage.readFromParquet[Product](s)
+      case None ⇒
+        log.warn(s"No existing integrated file specified -- regarding as initial load.")
+        spark.emptyDataset[Product]
+    }
+
+    val transformed = transform(spark, products, previousIntegrated)
+
+    storage.writeToSingleCsv(transformed, config.outputFile, delim = outputCsvDelimiter, quote = outputCsvQuote)
+  }
+
+  def createUfsProducts(spark: SparkSession, products: Dataset[Product]): Dataset[UFSProduct] = {
     import spark.implicits._
 
     products.map { product ⇒
@@ -23,17 +58,5 @@ object ProductAcmConverter extends SparkJobWithDefaultConfig with AcmTransformat
         DELETE_FLAG = Some(if (product.isActive) "N" else "Y")
       )
     }
-  }
-
-  override def run(spark: SparkSession, config: DefaultConfig, storage: Storage): Unit = {
-    import spark.implicits._
-
-    log.info(s"Generating products ACM csv file from [${config.inputFile}] to [${config.outputFile}]")
-
-    val products = storage.readFromParquet[Product](config.inputFile)
-
-    val transformed = transform(spark, products)
-
-    storage.writeToSingleCsv(transformed, config.outputFile, delim = outputCsvDelimiter, quote = outputCsvQuote)
   }
 }
