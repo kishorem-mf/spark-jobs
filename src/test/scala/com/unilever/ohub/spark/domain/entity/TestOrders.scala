826a5e1a0e5353d41923cab9810c0ac439e9d29d
Tim van Cann <timvancann@godatadriven.com>
2018-06-11 15:55:24 +0200

    OHUB-587: Add missing files

---
+++ src/test/scala/com/unilever/ohub/spark/domain/entity/TestOrders.scala
@@ -0,0 +1,45 @@
+package com.unilever.ohub.spark.domain.entity
+
+import java.sql.Timestamp
+
+object TestOrders extends TestOrders
+
+trait TestOrders {
+
+  // format: OFF
+  lazy val defaultOrder: Order = Order(
+    concatId = "country-code~source-name~source-entity-id",
+    countryCode = "country-code",
+    customerType = Order.customerType,
+    dateCreated = None,
+    dateUpdated = None,
+    isActive = true,
+    isGoldenRecord = true,
+    ohubId = Some("ohub-id"),
+    sourceEntityId = "source-entity-id",
+    sourceName = "source-name",
+    ohubCreated = new Timestamp(System.currentTimeMillis()),
+    ohubUpdated = new Timestamp(System.currentTimeMillis()),
+    // specific fields
+    `type` = Some("DIRECT"),
+    campaignCode = Some("UNKNOWN"),
+    campaignName = Some("campaign"),
+    comment = None,
+    contactPersonConcatId = Some("some~contact~person"),
+    contactPersonOhubId = None,
+    distributorId = None,
+    distributorLocation = None,
+    distributorName = Some("Van der Valk"),
+    distributorOperatorId = None,
+    operatorConcatId = Some("some~operator~id"),
+    operatorOhubId = None,
+    transactionDate = Some(new Timestamp(System.currentTimeMillis())),
+    vat = None,
+    // other fields
+    additionalFields = Map(),
+    ingestionErrors = Map()
+  )
+
+  // format: ON
+}
+
