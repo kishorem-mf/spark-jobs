ad5c4206414ef8a43f51cc5f6fa58fb883666043
Tycho Grouwstra <Tycho.Grouwstra@unilever.com>
2018-07-09 17:15:32 +0200

    more WasbCopy logging

--- dags/custom_operators/wasb_copy.py
+++ dags/custom_operators/wasb_copy.py
@@ -1,6 +1,7 @@
 from custom_operators.wasb_hook import WasbHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+import json
 
 
 class WasbCopyOperator(BaseOperator):
@@ -20,11 +21,26 @@ class WasbCopyOperator(BaseOperator):
 
     def execute(self, context):
         """Copy a file in Azure Blob Storage."""
+
+        print("self.wasb_conn_id")
+        print(self.wasb_conn_id)
+
         hook = WasbHook(wasb_conn_id=self.wasb_conn_id)
+        print("hook")
+        print(json.dumps(vars(hook)))
+
+        print("self.container_name")
+        print(self.container_name)
+
+        print("self.copy_source")
+        print(self.copy_source)
+
         print("confirming source")
         assert hook.check_for_blob(self.container_name, self.copy_source)
+
         print("copying " + self.copy_source +  " in container " + self.container_name + " to " + self.blob_name)
         hook.copy_blob(self.container_name, self.blob_name, self.copy_source)
+
         print("confirming destination")
         assert hook.check_for_blob(self.container_name, self.blob_name)
         print("confirmed destination")
