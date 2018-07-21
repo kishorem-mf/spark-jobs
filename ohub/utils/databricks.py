839e6e03d74951d4e22dbf8d797050ba3184d7e1
Bas Harenslak <basharenslak@gmail.com>
2018-07-21 22:50:02 +0200

    Black formatting

--- ohub/utils/databricks.py
+++ ohub/utils/databricks.py
@@ -1,13 +1,12 @@
 import logging
-import time
 
 from airflow.contrib.hooks.databricks_hook import DatabricksHook
 from airflow.exceptions import AirflowException
 
 
-def find_running_clusters_by_name(cluster_name,
-                                  databricks_conn_id='databricks_default',
-                                  databricks_hook=None):
+def find_running_clusters_by_name(
+    cluster_name, databricks_conn_id="databricks_default", databricks_hook=None
+):
     """
     Queries the Databricks API to find all running clusters with the given cluster name.
     :param cluster_name: the cluster name to look for.
@@ -16,16 +15,20 @@ def find_running_clusters_by_name(cluster_name,
     :return: a list of ClusterInfo: https://docs.databricks.com/api/latest/clusters.html#clusterclusterinfo
     """
     hook = databricks_hook or DatabricksHook(databricks_conn_id=databricks_conn_id)
-    body = hook._do_api_call(('GET', 'api/2.0/clusters/list'), {})
+    body = hook._do_api_call(("GET", "api/2.0/clusters/list"), {})
 
-    non_terminated_states = ['PENDING', 'RUNNING', 'RESTARTING', 'RESIZING']
-    return [cluster for cluster in body['clusters']
-            if cluster_name == cluster['cluster_name'] and cluster['state'] in non_terminated_states]
+    non_terminated_states = ["PENDING", "RUNNING", "RESTARTING", "RESIZING"]
+    return [
+        cluster
+        for cluster in body["clusters"]
+        if cluster_name == cluster["cluster_name"]
+        and cluster["state"] in non_terminated_states
+    ]
 
 
-def find_cluster_id(cluster_name,
-                    databricks_conn_id='databricks_default',
-                    databricks_hook=None):
+def find_cluster_id(
+    cluster_name, databricks_conn_id="databricks_default", databricks_hook=None
+):
     """
     Finds the `cluster_id` for a running cluster named `cluster_name`.
     :param cluster_name: the cluster name to look for.
@@ -34,20 +37,27 @@ def find_cluster_id(cluster_name,
     :return: a cluster ID
     :raises AirflowException: if no matching cluster is found.
     """
-    clusters = find_running_clusters_by_name(cluster_name, databricks_conn_id, databricks_hook)
+    clusters = find_running_clusters_by_name(
+        cluster_name, databricks_conn_id, databricks_hook
+    )
 
     if len(clusters) == 0:
-        raise AirflowException('Found no running Databricks cluster named "{}".'.format(cluster_name))
+        raise AirflowException(
+            'Found no running Databricks cluster named "{}".'.format(cluster_name)
+        )
     elif len(clusters) > 1:
-        logging.warning('Found more than one running Databricks cluster named "{}", using first match.', cluster_name)
+        logging.warning(
+            'Found more than one running Databricks cluster named "{}", using first match.',
+            cluster_name,
+        )
 
-    cluster_id = clusters[0]['cluster_id']
+    cluster_id = clusters[0]["cluster_id"]
     return cluster_id
 
 
-def get_cluster_status(cluster_id,
-                       databricks_conn_id='databricks_default',
-                       databricks_hook=None):
+def get_cluster_status(
+    cluster_id, databricks_conn_id="databricks_default", databricks_hook=None
+):
     """
     Requests databricks for the status of the cluster. For full specification of the return json,
     see https://docs.databricks.com/api/latest/clusters.html#get
@@ -59,5 +69,7 @@ def get_cluster_status(cluster_id,
     """
 
     hook = databricks_hook or DatabricksHook(databricks_conn_id=databricks_conn_id)
-    body = hook._do_api_call(('GET', 'api/2.0/clusters/get?cluster_id={}'.format(cluster_id)), {})
-    return body['state']
+    body = hook._do_api_call(
+        ("GET", "api/2.0/clusters/get?cluster_id={}".format(cluster_id)), {}
+    )
+    return body["state"]
