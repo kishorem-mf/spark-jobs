9cb636ff8defbcf48865218d1e82ac763d32872b
Bas Harenslak <basharenslak@gmail.com>
2018-07-21 22:35:16 +0200

    Moved WasbCopyOperator to wasb_operator.py

--- ohub/operators/wasb_operator.py
+++ ohub/operators/wasb_operator.py
@@ -1,9 +1,11 @@
 # Adjusted from https://github.com/apache/incubator-airflow/blob/master/airflow/contrib/operators/file_to_wasb.py
 
 import os
-from airflow.contrib.hooks.wasb_hook import WasbHook
+
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+
+from ohub.vendor.airflow.contrib.hooks.wasb_hook import WasbHook
 
 
 class FolderToWasbOperator(BaseOperator):
@@ -47,3 +49,53 @@ class FolderToWasbOperator(BaseOperator):
             hook.load_file(
                 file_path, self._container_name, remote_path, **self._load_kwargs
             )
+
+
+class WasbCopyOperator(BaseOperator):
+    """
+    Copy a file in Azure Blob Storage.
+    """
+
+    template_fields = ("_container_name", "_blob_name", "_copy_source")
+
+    @apply_defaults
+    def __init__(self, wasb_conn_id, container_name, blob_name, copy_source, **kwargs):
+        super().__init__()
+        self._wasb_conn_id = wasb_conn_id
+        self._container_name = container_name
+        self._blob_name = blob_name
+        self._copy_source = copy_source
+        self._kwargs = kwargs
+
+    def execute(self, context):
+        """Copy a file in Azure Blob Storage."""
+
+        print("self._wasb_conn_id")
+        print(self._wasb_conn_id)
+
+        hook = WasbHook(wasb_conn_id=self._wasb_conn_id)
+        # print("hook")
+        # print(json.dumps(vars(hook)))
+
+        print("self._container_name")
+        print(self._container_name)
+
+        print("self._copy_source")
+        print(self._copy_source)
+
+        print("confirming source")
+        assert hook.check_for_blob(self._container_name, self._copy_source)
+
+        print(
+            "copying "
+            + self._copy_source
+            + " in container "
+            + self._container_name
+            + " to "
+            + self._blob_name
+        )
+        hook.copy_blob(self._container_name, self._blob_name, self._copy_source)
+
+        print("confirming destination")
+        assert hook.check_for_blob(self._container_name, self._blob_name)
+        print("confirmed destination")
