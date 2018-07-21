99f72af48effb75f3e27b42c467b7d58a400dadf
Bas Harenslak <basharenslak@gmail.com>
2018-07-21 22:51:51 +0200

    Fix comment, config -> connection

--- ohub/utils/airflow.py
+++ ohub/utils/airflow.py
@@ -2,7 +2,7 @@ from airflow.hooks.base_hook import BaseHook
 
 
 class LazyConnection(object):
-    """Lazy connection class that only fetches config when accessed."""
+    """Lazy connection class that only fetches connection when accessed."""
 
     def __init__(self, conn_id):
         self._conn_id = conn_id
