8cfb97ce765f507c5ca6968f779077d097fdc29d
Bas Harenslak <basharenslak@gmail.com>
2018-07-21 22:51:09 +0200

    Added Airflow utils

---
+++ ohub/utils/airflow.py
@@ -0,0 +1,14 @@
+from airflow.hooks.base_hook import BaseHook
+
+
+class LazyConnection(object):
+    """Lazy connection class that only fetches config when accessed."""
+
+    def __init__(self, conn_id):
+        self._conn_id = conn_id
+        self._conn = None
+
+    def __getattr__(self):
+        if self._conn is None:
+            self._conn = BaseHook.get_connection(self._conn_id)
+        return getattr(self, self._conn)
