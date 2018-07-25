e344d42ae0f83203f1f9b3dfc37d55b1faba3625
Bas Harenslak <basharenslak@gmail.com>
2018-07-25 09:27:00 +0200

    Refactor Airflow LazyConnection

--- ohub/utils/airflow.py
+++ ohub/utils/airflow.py
@@ -5,23 +5,17 @@ from airflow.models import BaseOperator
 class LazyConnection(object):
     """Lazy connection class that only fetches connection when accessed.
 
-    :param str conn_id: Airflow connection id.
+    :param str _conn_id: Airflow connection id.
     """
 
     def __init__(self, conn_id: str):
-        self.conn = None
+        self._conn = None
         self._conn_id = conn_id
 
-    def __getattribute__(self, item):
-        if object.__getattribute__(self, "conn") is None:
-            object.__setattr__(
-                self,
-                "conn",
-                BaseHook.get_connection(
-                    conn_id=object.__getattribute__(self, "_conn_id")
-                ),
-            )
-        return object.__getattribute__(self, item)
+    def __getattr__(self, name):
+        if self._conn is None:
+            self._conn = BaseHook.get_connection(conn_id=self._conn_id)
+        return getattr(self._conn, name)
 
 
 class SubPipeline(object):
