6c89d6ccd0c754dfcafd932332ad99941e5f5bfe
Bas Harenslak <basharenslak@gmail.com>
2018-07-25 10:24:34 +0200

    Refactor fuzzit DAG. Black formatting, local var naming

--- dags/fuzzit_sftp_dag.py
+++ dags/fuzzit_sftp_dag.py
@@ -9,46 +9,46 @@ from ohub.operators.short_circuit_sftp_operator import ShortCircuitSFTPOperator
 from dags.ohub_dag_config import container_name
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2018, 3, 2),
-    'email': ['airflow@airflow.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 0
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2018, 3, 2),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 0,
 }
 
-fds = "{{macros.ds_format(ds, '%Y-%m-%d', '%Y%m%d')}}"
-templated_remote_filepath = "/ftp/ftp_ohub20/UFS_Fuzzit_OHUB20_" + fds + "_1400.zip"
-templated_local_filepath = "/tmp/fuzzit/{{ds}}/UFS_Fuzzit_OHUB20_1400.zip"
-templated_path_to_unzip_contents = '/tmp/fuzzit/{{ds}}/csv/'
-
-with DAG('fuzzit_sftp', default_args=default_args,
-         schedule_interval="0 0 1 * *") as dag:
+with DAG("fuzzit_sftp", default_args=default_args, schedule_interval="0 0 1 * *") as dag:
+    fds = "{{ macros.ds_format(ds, '%Y-%m-%d', '%Y%m%d') }}"
+    remote_filepath = f"/ftp/ftp_ohub20/UFS_Fuzzit_OHUB20_{fds}_1400.zip"
+    local_filepath = "/tmp/fuzzit/{{ ds }}/UFS_Fuzzit_OHUB20_1400.zip"
+    path_to_unzip_contents = "/tmp/fuzzit/{{ ds }}/csv/"
 
     mkdir = BashOperator(
-        bash_command='mkdir -p ' + templated_path_to_unzip_contents,
-        task_id='mkdir_fuzzit',
+        bash_command=f"mkdir -p {path_to_unzip_contents}",
+        task_id="mkdir_fuzzit",
     )
 
     fetch = ShortCircuitSFTPOperator(
-        task_id='fetch_fuzzit_files_for_date',
-        ssh_conn_id='fuzzit_sftp_ssh',
-        remote_host='apps.systrion.eu',
-        local_filepath=templated_local_filepath,
-        remote_filepath=templated_remote_filepath,
-        operation=SFTPOperation.GET)
+        task_id="fetch_fuzzit_files_for_date",
+        ssh_conn_id="fuzzit_sftp_ssh",
+        remote_host="apps.systrion.eu",
+        local_filepath=local_filepath,
+        remote_filepath=remote_filepath,
+        operation=SFTPOperation.GET,
+    )
 
     unzip = UnzipOperator(
-        task_id='unzip_fuzzit_file',
-        path_to_zip_file=templated_local_filepath,
-        path_to_unzip_contents=templated_path_to_unzip_contents,
-        dag=dag)
+        task_id="unzip_fuzzit_file",
+        path_to_zip_file=local_filepath,
+        path_to_unzip_contents=path_to_unzip_contents,
+        dag=dag,
+    )
 
     wasb = FolderToWasbOperator(
-        task_id='fuzzit_to_wasb',
-        folder_path=templated_path_to_unzip_contents,
-        blob_name='data/raw/fuzzit/{{ds}}',
+        task_id="fuzzit_to_wasb",
+        folder_path=path_to_unzip_contents,
+        blob_name="data/raw/fuzzit/{{ds}}",
         container_name=container_name,
     )
 
