36d3a886b448e42e1a52e785ef1788603a99ddbc
Bas Harenslak <basharenslak@gmail.com>
2018-07-25 09:26:11 +0200

    Comply with pylint

--- tests/ohub/utils/test_airflow.py
+++ tests/ohub/utils/test_airflow.py
@@ -1,17 +1,21 @@
-import pytest
+"""Tests for `ohub.utils.airflow`"""
+
 from airflow.hooks.base_hook import BaseHook
 
 from ohub.utils.airflow import LazyConnection
 
 
-class TestLazyConnection(object):
-    def test_no_connection_fetched(self, mocker):
-        mock = mocker.patch.object(BaseHook, "get_connection")
-        lazy_conn = LazyConnection(conn_id="test")
-        mock.assert_not_called()
+def test_no_connection_fetched(mocker):
+    """Test if LazyConnection initialization results in call to get_connection()"""
+    mock = mocker.patch.object(BaseHook, "get_connection")
+    LazyConnection(conn_id="test")
+    mock.assert_not_called()
 
-    def test_connection_fetched(self, mocker):
-        mock = mocker.patch.object(BaseHook, "get_connection")
-        lazy_conn = LazyConnection(conn_id="test")
-        password = lazy_conn.conn.password
-        mock.assert_called_once_with(conn_id="test")
+
+def test_connection_fetched(mocker):
+    """Test if calling LazyConnection attribute results in call to get_connection()"""
+    mock = mocker.patch.object(BaseHook, "get_connection")
+    lazy_conn = LazyConnection(conn_id="test")
+    # pylint: disable=pointless-statement
+    lazy_conn.password
+    mock.assert_called_once_with(conn_id="test")
