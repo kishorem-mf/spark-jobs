c665727ca6a027aff9e928eb918816a8006c2e59
Tycho Grouwstra <Tycho.Grouwstra@unilever.com>
2018-08-04 16:15:33 +0200

    use match consutrction to deal with 22 field limit

--- src/main/scala/com/unilever/ohub/spark/acm/ContactPersonAcmConverter.scala
+++ src/main/scala/com/unilever/ohub/spark/acm/ContactPersonAcmConverter.scala
@@ -56,66 +56,68 @@ object ContactPersonAcmConverter extends SparkJob[DefaultWithDeltaConfig]
   ): Dataset[AcmContactPerson] = {
     import spark.implicits._
 
-    contactPersons.filter(_.isGoldenRecord).map { contactPerson ⇒ // TODO check whether the filter is at the right location
-      val (concatId, countryCode, customerType, dateCreated, dateUpdated, isActive, isGoldenRecord, ohubId, name, sourceEntityId, sourceName, ohubCreated, ohubUpdated, operatorConcatId, operatorOhubId, oldIntegrationId, firstName, lastName, title, gender, jobTitle, language, birthDate, street, houseNumber, houseNumberExtension, city, zipCode, state, countryName, isPreferredContact, isKeyDecisionMaker, standardCommunicationChannel, emailAddress, phoneNumber, mobileNumber, faxNumber, hasGeneralOptOut, hasConfirmedRegistration, confirmedRegistrationDate, hasEmailOptIn, emailOptInDate, hasEmailDoubleOptIn, emailDoubleOptInDate, hasEmailOptOut, hasDirectMailOptIn, hasDirectMailOptOut, hasTeleMarketingOptIn, hasTeleMarketingOptOut, hasMobileOptIn, mobileOptInDate, hasMobileDoubleOptIn, mobileDoubleOptInDate, hasMobileOptOut, hasFaxOptIn, hasFaxOptOut, webUpdaterId, additionalFields, ingestionErrors) = ContactPerson.unapply(contactPerson).get
-      AcmContactPerson(
-        CP_ORIG_INTEGRATION_ID = concatId,
-        CP_LNKD_INTEGRATION_ID = ohubId.get, // TODO resolve .get here...what if we don't have an ohubId?
-        OPR_ORIG_INTEGRATION_ID = oldIntegrationId.get, // TODO opr-ohub-id...add to domain (is set in the merging step)
-        GOLDEN_RECORD_FLAG = "Y",
-        WEB_CONTACT_ID = Option.empty,
-        EMAIL_OPTOUT = hasEmailOptOut.map(boolAsString),
-        PHONE_OPTOUT = hasTeleMarketingOptOut.map(boolAsString),
-        FAX_OPTOUT = hasFaxOptOut.map(boolAsString),
-        MOBILE_OPTOUT = hasMobileOptOut.map(boolAsString),
-        DM_OPTOUT = hasDirectMailOptOut.map(boolAsString),
-        LAST_NAME = lastName,
-        FIRST_NAME = Option(cleanNames(
-          firstName.getOrElse(""),
-          lastName.getOrElse("")
-        )),
-        MIDDLE_NAME = Option.empty,
-        TITLE = title,
-        GENDER = gender.map {
-          case "M" ⇒ "1"
-          case "F" ⇒ "2"
-          case _   ⇒ "0"
-        },
-        LANGUAGE = language,
-        EMAIL_ADDRESS = emailAddress,
-        MOBILE_PHONE_NUMBER = mobileNumber,
-        PHONE_NUMBER = phoneNumber,
-        FAX_NUMBER = faxNumber,
-        STREET = street.map(clean),
-        HOUSENUMBER = Option(Seq(houseNumber, houseNumberExtension).flatten.map(clean).mkString(" ")),
-        ZIPCODE = zipCode.map(clean),
-        CITY = city.map(clean),
-        COUNTRY = countryName,
-        DATE_CREATED = dateCreated.map(formatWithPattern()),
-        DATE_UPDATED = dateUpdated.map(formatWithPattern()),
-        DATE_OF_BIRTH = birthDate.map(formatWithPattern()),
-        PREFERRED = isPreferredContact.map(boolAsString),
-        ROLE = jobTitle,
-        COUNTRY_CODE = Some(countryCode),
-        SCM = standardCommunicationChannel,
-        DELETE_FLAG = if (isActive) "0" else "1",
-        KEY_DECISION_MAKER = isKeyDecisionMaker.map(boolAsString),
-        OPT_IN = hasEmailOptIn.map(boolAsString),
-        OPT_IN_DATE = emailOptInDate.map(formatWithPattern()),
-        CONFIRMED_OPT_IN = hasConfirmedRegistration.map(boolAsString),
-        CONFIRMED_OPT_IN_DATE = confirmedRegistrationDate.map(formatWithPattern()),
-        MOB_OPT_IN = hasMobileOptIn.map(boolAsString),
-        MOB_OPT_IN_DATE = mobileOptInDate.map(formatWithPattern()),
-        MOB_CONFIRMED_OPT_IN = hasMobileDoubleOptIn.map(boolAsString),
-        MOB_CONFIRMED_OPT_IN_DATE = mobileDoubleOptInDate.map(formatWithPattern()),
-        MOB_OPT_OUT_DATE = Option.empty,
-        ORG_FIRST_NAME = firstName,
-        ORG_LAST_NAME = lastName,
-        ORG_EMAIL_ADDRESS = emailAddress,
-        ORG_FIXED_PHONE_NUMBER = phoneNumber,
-        ORG_MOBILE_PHONE_NUMBER = mobileNumber,
-        ORG_FAX_NUMBER = faxNumber
-      )
+    contactPersons.filter(_.isGoldenRecord).map { cp ⇒
+      // TODO check whether the filter is at the right location
+      cp match {
+        case ContactPerson(concatId, countryCode, customerType, dateCreated, dateUpdated, isActive, isGoldenRecord, ohubId, name, sourceEntityId, sourceName, ohubCreated, ohubUpdated, operatorConcatId, operatorOhubId, oldIntegrationId, firstName, lastName, title, gender, jobTitle, language, birthDate, street, houseNumber, houseNumberExtension, city, zipCode, state, countryName, isPreferredContact, isKeyDecisionMaker, standardCommunicationChannel, emailAddress, phoneNumber, mobileNumber, faxNumber, hasGeneralOptOut, hasConfirmedRegistration, confirmedRegistrationDate, hasEmailOptIn, emailOptInDate, hasEmailDoubleOptIn, emailDoubleOptInDate, hasEmailOptOut, hasDirectMailOptIn, hasDirectMailOptOut, hasTeleMarketingOptIn, hasTeleMarketingOptOut, hasMobileOptIn, mobileOptInDate, hasMobileDoubleOptIn, mobileDoubleOptInDate, hasMobileOptOut, hasFaxOptIn, hasFaxOptOut, webUpdaterId, additionalFields, ingestionErrors) => AcmContactPerson(
+          CP_ORIG_INTEGRATION_ID = concatId,
+          CP_LNKD_INTEGRATION_ID = ohubId.get, // TODO resolve .get here...what if we don't have an ohubId?
+          OPR_ORIG_INTEGRATION_ID = oldIntegrationId.get, // TODO opr-ohub-id...add to domain (is set in the merging step)
+          GOLDEN_RECORD_FLAG = "Y",
+          WEB_CONTACT_ID = Option.empty,
+          EMAIL_OPTOUT = hasEmailOptOut.map(boolAsString),
+          PHONE_OPTOUT = hasTeleMarketingOptOut.map(boolAsString),
+          FAX_OPTOUT = hasFaxOptOut.map(boolAsString),
+          MOBILE_OPTOUT = hasMobileOptOut.map(boolAsString),
+          DM_OPTOUT = hasDirectMailOptOut.map(boolAsString),
+          LAST_NAME = lastName,
+          FIRST_NAME = Option(cleanNames(
+            firstName.getOrElse(""),
+            lastName.getOrElse("")
+          )),
+          MIDDLE_NAME = Option.empty,
+          TITLE = title,
+          GENDER = gender.map {
+            case "M" ⇒ "1"
+            case "F" ⇒ "2"
+            case _   ⇒ "0"
+          },
+          LANGUAGE = language,
+          EMAIL_ADDRESS = emailAddress,
+          MOBILE_PHONE_NUMBER = mobileNumber,
+          PHONE_NUMBER = phoneNumber,
+          FAX_NUMBER = faxNumber,
+          STREET = street.map(clean),
+          HOUSENUMBER = Option(Seq(houseNumber, houseNumberExtension).flatten.map(clean).mkString(" ")),
+          ZIPCODE = zipCode.map(clean),
+          CITY = city.map(clean),
+          COUNTRY = countryName,
+          DATE_CREATED = dateCreated.map(formatWithPattern()),
+          DATE_UPDATED = dateUpdated.map(formatWithPattern()),
+          DATE_OF_BIRTH = birthDate.map(formatWithPattern()),
+          PREFERRED = isPreferredContact.map(boolAsString),
+          ROLE = jobTitle,
+          COUNTRY_CODE = Some(countryCode),
+          SCM = standardCommunicationChannel,
+          DELETE_FLAG = if (isActive) "0" else "1",
+          KEY_DECISION_MAKER = isKeyDecisionMaker.map(boolAsString),
+          OPT_IN = hasEmailOptIn.map(boolAsString),
+          OPT_IN_DATE = emailOptInDate.map(formatWithPattern()),
+          CONFIRMED_OPT_IN = hasConfirmedRegistration.map(boolAsString),
+          CONFIRMED_OPT_IN_DATE = confirmedRegistrationDate.map(formatWithPattern()),
+          MOB_OPT_IN = hasMobileOptIn.map(boolAsString),
+          MOB_OPT_IN_DATE = mobileOptInDate.map(formatWithPattern()),
+          MOB_CONFIRMED_OPT_IN = hasMobileDoubleOptIn.map(boolAsString),
+          MOB_CONFIRMED_OPT_IN_DATE = mobileDoubleOptInDate.map(formatWithPattern()),
+          MOB_OPT_OUT_DATE = Option.empty,
+          ORG_FIRST_NAME = firstName,
+          ORG_LAST_NAME = lastName,
+          ORG_EMAIL_ADDRESS = emailAddress,
+          ORG_FIXED_PHONE_NUMBER = phoneNumber,
+          ORG_MOBILE_PHONE_NUMBER = mobileNumber,
+          ORG_FAX_NUMBER = faxNumber
+        )
+      }
     }
   }
 
--- src/main/scala/com/unilever/ohub/spark/dispatcher/OperatorDispatcherConverter.scala
+++ src/main/scala/com/unilever/ohub/spark/dispatcher/OperatorDispatcherConverter.scala
@@ -62,70 +62,71 @@ object OperatorDispatcherConverter extends SparkJob[DefaultWithDeltaConfig]
     import spark.implicits._
 
     val dispatcherOperators = operators.map { op ⇒
-      val (concatId, countryCode, customerType, dateCreated, dateUpdated, isActive, isGoldenRecord, ohubId, name, sourceEntityId, sourceName, ohubCreated, ohubUpdated, averagePrice, chainId, chainName, channel, city, cookingConvenienceLevel, countryName, daysOpen, distributorName, distributorOperatorId, emailAddress, faxNumber, hasDirectMailOptIn, hasDirectMailOptOut, hasEmailOptIn, hasEmailOptOut, hasFaxOptIn, hasFaxOptOut, hasGeneralOptOut, hasMobileOptIn, hasMobileOptOut, hasTelemarketingOptIn, hasTelemarketingOptOut, houseNumber, houseNumberExtension, isNotRecalculatingOtm, isOpenOnFriday, isOpenOnMonday, isOpenOnSaturday, isOpenOnSunday, isOpenOnThursday, isOpenOnTuesday, isOpenOnWednesday, isPrivateHousehold, kitchenType, mobileNumber, netPromoterScore, oldIntegrationId, otm, otmEnteredBy, phoneNumber, region, salesRepresentative, state, street, subChannel, totalDishes, totalLocations, totalStaff, vat, webUpdaterId, weeksClosed, zipCode, additionalFields, ingestionErrors) = Operator.unapply(op).get
-      DispatcherOperator(
-        OPR_ORIG_INTEGRATION_ID = concatId,
-        OPR_LNKD_INTEGRATION_ID = ohubId,
-        AVERAGE_SELLING_PRICE = averagePrice,
-        CHAIN_KNOTEN = chainId,
-        CHAIN_NAME = chainName,
-        CHANNEL = channel,
-        CITY = city,
-        CONVENIENCE_LEVEL = cookingConvenienceLevel,
-        COUNTRY_CODE = countryCode,
-        COUNTRY = countryName,
-        NUMBER_OF_DAYS_OPEN = daysOpen,
-        WHOLE_SALER_OPERATOR_ID = distributorOperatorId,
-        DM_OPT_OUT = hasDirectMailOptOut,
-        EMAIL_OPT_OUT = hasEmailOptOut,
-        FAX_OPT_OUT = hasFaxOptOut,
-        MOBILE_OPT_OUT = hasMobileOptOut,
-        FIXED_OPT_OUT = hasTelemarketingOptOut,
-        HOUSE_NUMBER = houseNumber,
-        HOUSE_NUMBER_EXT = houseNumberExtension,
-        DELETE_FLAG = !isActive,
-        GOLDEN_RECORD_FLAG = isGoldenRecord,
-        OPEN_ON_FRIDAY = isOpenOnFriday,
-        OPEN_ON_MONDAY = isOpenOnMonday,
-        OPEN_ON_SATURDAY = isOpenOnSaturday,
-        OPEN_ON_SUNDAY = isOpenOnSunday,
-        OPEN_ON_THURSDAY = isOpenOnThursday,
-        OPEN_ON_TUESDAY = isOpenOnTuesday,
-        OPEN_ON_WEDNESDAY = isOpenOnWednesday,
-        PRIVATE_HOUSEHOLD = isPrivateHousehold,
-        KITCHEN_TYPE = kitchenType,
-        NAME = name,
-        NPS_POTENTIAL = netPromoterScore,
-        CREATED_AT = formatWithPattern()(ohubCreated),
-        UPDATED_AT = formatWithPattern()(ohubUpdated),
-        OTM = otm,
-        REGION = region,
-        SOURCE_ID = sourceEntityId,
-        SOURCE = sourceName,
-        STATE = state,
-        STREET = street,
-        SUB_CHANNEL = subChannel,
-        NUMBER_OF_COVERS = totalDishes,
-        VAT = vat,
-        NUMBER_OF_WEEKS_OPEN = weeksClosed.map { weeksClosed ⇒
-          if (52 - weeksClosed < 0) 0 else 52 - weeksClosed
-        },
-        ZIP_CODE = zipCode,
-        ROUTE_TO_MARKET = None,
-        PREFERRED_PARTNER = Some("-2"),
-        STATUS = None, // "status of the operator, for example: seasonal, D, A, closed
-        RESPONSIBLE_EMPLOYEE = None,
-        CAM_KEY = None,
-        CAM_TEXT = None,
-        CHANNEL_KEY = None,
-        CHANNEL_TEXT = None,
-        LOCAL_CHANNEL = None, // set below
-        CHANNEL_USAGE = None, // set below
-        SOCIAL_COMMERCIAL = None, // set below
-        STRATEGIC_CHANNEL = None, // set below
-        GLOBAL_CHANNEL = None, // set below
-        GLOBAL_SUBCHANNEL = None // set below
-      )
+        op match {
+          case Operator(concatId, countryCode, customerType, dateCreated, dateUpdated, isActive, isGoldenRecord, ohubId, name, sourceEntityId, sourceName, ohubCreated, ohubUpdated, averagePrice, chainId, chainName, channel, city, cookingConvenienceLevel, countryName, daysOpen, distributorName, distributorOperatorId, emailAddress, faxNumber, hasDirectMailOptIn, hasDirectMailOptOut, hasEmailOptIn, hasEmailOptOut, hasFaxOptIn, hasFaxOptOut, hasGeneralOptOut, hasMobileOptIn, hasMobileOptOut, hasTelemarketingOptIn, hasTelemarketingOptOut, houseNumber, houseNumberExtension, isNotRecalculatingOtm, isOpenOnFriday, isOpenOnMonday, isOpenOnSaturday, isOpenOnSunday, isOpenOnThursday, isOpenOnTuesday, isOpenOnWednesday, isPrivateHousehold, kitchenType, mobileNumber, netPromoterScore, oldIntegrationId, otm, otmEnteredBy, phoneNumber, region, salesRepresentative, state, street, subChannel, totalDishes, totalLocations, totalStaff, vat, webUpdaterId, weeksClosed, zipCode, additionalFields, ingestionErrors) => DispatcherOperator(
+          OPR_ORIG_INTEGRATION_ID = concatId,
+          OPR_LNKD_INTEGRATION_ID = ohubId,
+          AVERAGE_SELLING_PRICE = averagePrice,
+          CHAIN_KNOTEN = chainId,
+          CHAIN_NAME = chainName,
+          CHANNEL = channel,
+          CITY = city,
+          CONVENIENCE_LEVEL = cookingConvenienceLevel,
+          COUNTRY_CODE = countryCode,
+          COUNTRY = countryName,
+          NUMBER_OF_DAYS_OPEN = daysOpen,
+          WHOLE_SALER_OPERATOR_ID = distributorOperatorId,
+          DM_OPT_OUT = hasDirectMailOptOut,
+          EMAIL_OPT_OUT = hasEmailOptOut,
+          FAX_OPT_OUT = hasFaxOptOut,
+          MOBILE_OPT_OUT = hasMobileOptOut,
+          FIXED_OPT_OUT = hasTelemarketingOptOut,
+          HOUSE_NUMBER = houseNumber,
+          HOUSE_NUMBER_EXT = houseNumberExtension,
+          DELETE_FLAG = !isActive,
+          GOLDEN_RECORD_FLAG = isGoldenRecord,
+          OPEN_ON_FRIDAY = isOpenOnFriday,
+          OPEN_ON_MONDAY = isOpenOnMonday,
+          OPEN_ON_SATURDAY = isOpenOnSaturday,
+          OPEN_ON_SUNDAY = isOpenOnSunday,
+          OPEN_ON_THURSDAY = isOpenOnThursday,
+          OPEN_ON_TUESDAY = isOpenOnTuesday,
+          OPEN_ON_WEDNESDAY = isOpenOnWednesday,
+          PRIVATE_HOUSEHOLD = isPrivateHousehold,
+          KITCHEN_TYPE = kitchenType,
+          NAME = name,
+          NPS_POTENTIAL = netPromoterScore,
+          CREATED_AT = formatWithPattern()(ohubCreated),
+          UPDATED_AT = formatWithPattern()(ohubUpdated),
+          OTM = otm,
+          REGION = region,
+          SOURCE_ID = sourceEntityId,
+          SOURCE = sourceName,
+          STATE = state,
+          STREET = street,
+          SUB_CHANNEL = subChannel,
+          NUMBER_OF_COVERS = totalDishes,
+          VAT = vat,
+          NUMBER_OF_WEEKS_OPEN = weeksClosed.map { weeksClosed ⇒
+            if (52 - weeksClosed < 0) 0 else 52 - weeksClosed
+          },
+          ZIP_CODE = zipCode,
+          ROUTE_TO_MARKET = None,
+          PREFERRED_PARTNER = Some("-2"),
+          STATUS = None, // "status of the operator, for example: seasonal, D, A, closed
+          RESPONSIBLE_EMPLOYEE = None,
+          CAM_KEY = None,
+          CAM_TEXT = None,
+          CHANNEL_KEY = None,
+          CHANNEL_TEXT = None,
+          LOCAL_CHANNEL = None, // set below
+          CHANNEL_USAGE = None, // set below
+          SOCIAL_COMMERCIAL = None, // set below
+          STRATEGIC_CHANNEL = None, // set below
+          GLOBAL_CHANNEL = None, // set below
+          GLOBAL_SUBCHANNEL = None // set below
+        )
+      }
     }
 
     dispatcherOperators
