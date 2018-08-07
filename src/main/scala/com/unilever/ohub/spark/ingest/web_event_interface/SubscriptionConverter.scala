bd126ef2b25939fdef0af98a8f8a95ee6c1b1f02
shivajimadhusoodhan <shivajimadhusoodhan@gmail.com>
2018-08-07 13:24:25 +0200

    as part of OHUB-1087-subscriptions-data-correctly-c

--- src/main/scala/com/unilever/ohub/spark/ingest/web_event_interface/SubscriptionConverter.scala
+++ src/main/scala/com/unilever/ohub/spark/ingest/web_event_interface/SubscriptionConverter.scala
@@ -1,10 +1,13 @@
 package com.unilever.ohub.spark.ingest.web_event_interface
 
+import java.sql.Timestamp
+
 import com.unilever.ohub.spark.domain.entity.Subscription
-import com.unilever.ohub.spark.ingest.{ DomainTransformer, SubscriptionEmptyParquetWriter }
+import com.unilever.ohub.spark.ingest.{DomainTransformer, SubscriptionEmptyParquetWriter}
 import org.apache.spark.sql.Row
 import com.unilever.ohub.spark.domain.DomainEntity
 import cats.syntax.option._
+import com.unilever.ohub.spark.ingest.CustomParsers.{parseBoolUnsafe, parseDateTimeStampUnsafe}
 
 /**
  * Placeholder object for Subscription
@@ -16,25 +19,35 @@ object SubscriptionConverter extends WebEventDomainGateKeeper[Subscription] with
     val countryCode: String = mandatoryValue("countryCode", "countryCode")
     val sourceEntityId: String = mandatoryValue("sourceId", "sourceEntityId")
     val concatId: String = DomainEntity.createConcatIdFromValues(countryCode, SourceName, sourceEntityId)
+    val contactPersonRefId = mandatory("contactPersonRefId", "contactPersonConcatId")
+    val contactPersonConcatId: String = DomainEntity.createConcatIdFromValues(countryCode,sourceEntityId, contactPersonRefId)
     val ohubCreated = currentTimestamp()
 
       // format: OFF
 
       Subscription(
-        concatId = concatId,
-        countryCode = countryCode,
-        customerType = Subscription.customerType,
-        dateCreated = ohubCreated,
-        dateUpdated = ohubCreated,
-        isActive = false,
-        isGoldenRecord = false,
-        sourceEntityId = sourceEntityId,
-        sourceName = SourceName,
-        ohubId = none,
-        ohubCreated = ohubCreated,
-        ohubUpdated = ohubCreated,
-        additionalFields = additionalFields,
-        ingestionErrors = errors
+        // fieldName                  mandatory   sourceFieldName             targetFieldName                 transformationFunction (unsafe)
+        concatId                =     concatId,
+        countryCode             =     countryCode,
+        customerType            =     Subscription.customerType,
+        dateCreated             =     None,
+        dateUpdated             =     None,
+        isActive                =     true,
+        isGoldenRecord          =     false,
+        sourceEntityId          =     sourceEntityId,
+        sourceName              =     SourceName,
+        ohubId                  =     none,
+        ohubCreated             =     ohubCreated,
+        ohubUpdated             =     ohubCreated,
+        contactPersonConcatId   =     contactPersonConcatId,
+        communicationChannel    =     mandatory(  "communicationChannel",     "communicationChannel"),
+        subscriptionType        =     mandatory(  "subscriptionType" ,        "subscriptionType"),
+        hasSubscription         =     mandatory(  "subscribed",               "hasSubscription",                    parseBoolUnsafe),
+        subscriptionDate        =     mandatory(  "subscribedDate",           "subscriptionDate",                   parseDateTimeStampUnsafe),
+        hasConfirmedSubscription =    optional(   "subscriptionConfirmed",    "hasConfirmedSubscription",           parseBoolUnsafe),
+        ConfirmedSubscriptionDate =   optional(   "subscriptionConfirmedDate", "ConfirmedSubscriptionDate",         parseDateTimeStampUnsafe),
+        additionalFields          =   additionalFields,
+        ingestionErrors           =   errors
       )
 
     // format: ON
