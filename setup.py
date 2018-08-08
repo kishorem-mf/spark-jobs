adc1d350141195d905e93fc692e67c48bd7d07cc
Bas Harenslak <basharenslak@gmail.com>
2018-08-08 23:10:04 +0200

    Added setup.py requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 import setuptools
 
-REQUIREMENTS = ["apache-airflow[crypto]>=1.9.0,<2", "paramiko>=2.4.1,<3"]
+REQUIREMENTS = [
+    "apache-airflow[crypto]>=1.9.0,<2",
+    "azure-storage-blob>=1.3.1,<2",
+    "azure>=1.0.2,<2",
+    "google-api-python-client>=1.7.4,<2",
+    "numpy~=1.14.0",
+    "oauth2client>=4.1.2,<5",
+    "pandas-gbq>=0.5.0,<1",
+    "paramiko>=2.4.1,<3",
+]
 
 EXTRAS_REQUIRE = {
     "dev": [
