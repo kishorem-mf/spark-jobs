0f3441d5ccce75146a704163cce599216052aae0
Bas Harenslak <basharenslak@gmail.com>
2018-08-08 16:07:13 +0200

    Replace the test.sh bash script by pytest module

--- Makefile
+++ Makefile
@@ -2,3 +2,10 @@
 lint:
 	bash ./scripts/pylint.sh --rcfile=.pylintrc ./ohub
 #	./dags was excluded for the moment because of ton of errors
+
+.PHONY: pytest
+pytest:
+	pytest tests/ -s
+
+.PHONY: local-ci
+local-ci: | lint pytest
--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import setuptools
 
+EXTRAS_REQUIRE = {"dev": ["pytest", "pytest-mock"]}
+
 setuptools.setup(
     name="ohub",
     version="0.1",
     python_requires=">=3.6",
     description="Unilever OHUB2.0 Airflow",
     packages=setuptools.find_packages(),
+    extras_require=EXTRAS_REQUIRE,
 )
