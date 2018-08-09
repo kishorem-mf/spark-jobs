128e2dbc15ad5298ed0a7779fa8cd5e204edbda3
shivajimadhusoodhan <shivajimadhusoodhan@gmail.com>
2018-08-09 11:10:02 +0200

    as part of OHUB-1087-subscriptions-data-correctly-c

--- src/test/scala/com/unilever/ohub/spark/ingest/web_event_interface/SubscriptionConverterSpec.scala
+++ src/test/scala/com/unilever/ohub/spark/ingest/web_event_interface/SubscriptionConverterSpec.scala
@@ -1,8 +1,10 @@
 package com.unilever.ohub.spark.ingest.web_event_interface
 
-import com.unilever.ohub.spark.domain.entity.{ ContactPerson, Subscription }
+import java.sql.Timestamp
+
+import com.unilever.ohub.spark.domain.entity.Subscription
 import com.unilever.ohub.spark.ingest.CsvDomainGateKeeperSpec
-import org.apache.spark.sql.Dataset
+
 
 class SubscriptionConverterSpec extends CsvDomainGateKeeperSpec[Subscription] {
   override val SUT = SubscriptionConverter
@@ -11,7 +13,36 @@ class SubscriptionConverterSpec extends CsvDomainGateKeeperSpec[Subscription] {
     it("should convert a subscription correctly from a valid web event csv input") {
       val inputFile = "src/test/resources/WEB_EVENT_SUBSCRIPTIONS.csv"
 
-      runJobWith(inputFile) { actualDataSet: Dataset[Subscription] ⇒
+      runJobWith(inputFile) { actualDataSet ⇒
+        actualDataSet.count() shouldBe 1
+
+        val actualSubscription = actualDataSet.head()
+        val expectedSubscription = Subscription(
+        concatId                =     "DE~EMAKINA~b3a6208c-d7f6-44e2-80e2-f26d461f64c0",
+        countryCode             =     "DE",
+        customerType            =     "SUBSCRIPTION",
+        dateCreated             =     None,
+        dateUpdated             =     None,
+        isActive                =     true,
+        isGoldenRecord          =     false,
+        sourceEntityId          =     "b3a6208c-d7f6-44e2-80e2-f26d461f64c0",
+        sourceName              =     "EMAKINA",
+        ohubId                  =     None,
+        ohubCreated             =     actualSubscription.ohubCreated,
+        ohubUpdated             =     actualSubscription.ohubCreated,
+        contactPersonConcatId   =     "DE~b3a6208c-d7f6-44e2-80e2-f26d461f64c0~138175",
+        communicationChannel    =     Some("Some channel"),
+        subscriptionType        =     "Newsletter",
+        hasSubscription         =     true,
+        subscriptionDate        =    actualSubscription.subscriptionDate,
+        hasConfirmedSubscription =   Some(true),
+        ConfirmedSubscriptionDate =  Some(Timestamp.valueOf("2018-07-17 15:51:30.0")),
+        additionalFields          =   Map(),
+        ingestionErrors           =   Map()
+        )
+
+      actualSubscription shouldBe expectedSubscription
+
         // placeholder for unit test
       }
     }
