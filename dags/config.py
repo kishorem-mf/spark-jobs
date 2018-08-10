262e1b3a65d2212ee5b32fd5575459b855ef8f6b
DivyaAvula05 <40656120+DivyaAvula05@users.noreply.github.com>
2018-08-10 20:01:53 +0530

    update DAG Dates

--- dags/config.py
+++ dags/config.py
@@ -23,8 +23,8 @@ ohub_entities = {
     "products": {},
 }
 
-start_date_first = datetime(2018, 8, 1)
-start_date_delta = datetime(2018, 8, 2)
+start_date_first = datetime(2018, 8, 8)
+start_date_delta = datetime(2018, 8, 9)
 
 country_codes = dict(
     AU=149299102,
