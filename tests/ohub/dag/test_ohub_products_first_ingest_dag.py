9184367687c6f71f7f95ac945df366eb3ac44fdf
Tycho Grouwstra <Tycho.Grouwstra@unilever.com>
2018-09-05 16:46:15 +0200

    pluralize ohub_product_first_ingest_dag

--- tests/ohub/dag/test_ohub_products_first_ingest_dag.py
+++ tests/ohub/dag/test_ohub_products_first_ingest_dag.py
@@ -2,6 +2,6 @@ from testing.utils.test import test_dag, TestCase
 
 
 class ProductInitialDagTestCase(TestCase):
-    def test_ohub_product_first_ingest_dag(self):
-        from dags.ohub_product_first_ingest_dag import dag
+    def test_ohub_products_first_ingest_dag(self):
+        from dags.ohub_products_first_ingest_dag import dag
         test_dag(dag)
