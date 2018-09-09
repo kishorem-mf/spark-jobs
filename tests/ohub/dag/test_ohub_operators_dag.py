f4b3996549ea26f3b9170540cfb2499bc3a17ac8
Tycho Grouwstra <Tycho.Grouwstra@unilever.com>
2018-09-09 21:26:59 +0200

    add operator delta test

--- tests/ohub/dag/test_ohub_operators_dag.py
+++ tests/ohub/dag/test_ohub_operators_dag.py
@@ -1,7 +1,9 @@
-# from testing.utils.test import test_dag, TestCase
+from testing.utils.test import test_dag, TestCase
 
 
-# class OperatorDeltaDagTestCase(TestCase):
-#     def test_ohub_operators_dag(self):
-#         from dags.ohub_operators_dag import dag
-#         test_dag(dag)
+class OperatorDeltaDagTestCase(TestCase):
+    def test_ohub_operators_dag(self):
+        from dags.ohub_operators_first_ingest_dag import dag as initial
+        from dags.ohub_operators_dag import dag as delta
+        test_dag(initial)
+        test_dag(delta)
