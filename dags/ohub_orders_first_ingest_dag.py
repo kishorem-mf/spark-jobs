9cd295d15a83040e79b2be1feeb764faca26409f
Tycho Grouwstra <Tycho.Grouwstra@unilever.com>
2018-09-09 15:56:03 +0200

    use single starting date

--- dags/ohub_orders_first_ingest_dag.py
+++ dags/ohub_orders_first_ingest_dag.py
@@ -9,9 +9,7 @@ from ohub.operators.external_task_sensor_operator import ExternalTaskSensorOpera
 from ohub.operators.wasb_operator import WasbCopyOperator
 from ohub.utils.airflow import DagConfig, GenericPipeline, SubPipeline
 
-dag_args = {**config.dag_default_args, **{
-        "start_date": datetime(2018, 7, 25),
-    }}
+dag_args = config.dag_default_args
 orders_entity = "orders"
 orders_dag_config = DagConfig(orders_entity, is_delta=False)
 orderlines_entity = "orderlines"
