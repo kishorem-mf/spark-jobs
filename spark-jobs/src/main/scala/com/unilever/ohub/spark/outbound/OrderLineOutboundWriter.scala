d9fb316cada37fb725afa6dadc29597bb136e047
Tycho Grouwstra <Tycho.Grouwstra@unilever.com>
2018-10-31 14:33:08 +0100

    add writer jobs for product, order, order line

---
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/outbound/OrderLineOutboundWriter.scala
@@ -0,0 +1,5 @@
+package com.unilever.ohub.spark.outbound
+
+import com.unilever.ohub.spark.domain.entity.OrderLine
+
+object OrderLineOutboundWriter extends DomainOutboundWriter[OrderLine]
---
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/outbound/ProductOutboundWriter.scala
@@ -0,0 +1,5 @@
+package com.unilever.ohub.spark.outbound
+
+import com.unilever.ohub.spark.domain.entity.Product
+
+object ProductOutboundWriter extends DomainOutboundWriter[Product]
