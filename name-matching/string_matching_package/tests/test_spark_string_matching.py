ed6201e5b1cf37a08f211432a7537a0f51a85c3a
Bas Harenslak <Bas.Harenslak@unilever.com>
2018-11-28 13:41:52 +0000

    Merged PR 2733: Add match_strings() validity test
    Add match_strings() validity test. This verifies if the number of similarity results is correct.
    
    Also: apply tests on spark_string_matching module, instead of alias victim.

--- name-matching/string_matching_package/tests/test_spark_string_matching.py
+++ name-matching/string_matching_package/tests/test_spark_string_matching.py
@@ -1,4 +1,5 @@
-from string_matching import spark_string_matching as victim
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from string_matching import spark_string_matching
 
 
 def gen_tuple(tup, n, start_idx=0):
@@ -16,15 +17,22 @@ class TestMatchStrings:
                 (2, 'ab'),
                 ]
         ddf = self.create_ddf(spark, data)
-        res = victim.match_strings(spark,
-                                   df=ddf,
-                                   string_column='matching_string',
-                                   row_number_column='id',
-                                   n_top=1500,
-                                   threshold=0.8,
-                                   n_gram=2,
-                                   min_document_frequency=2,
-                                   max_vocabulary_size=1500).select('i', 'j').sort('j', 'i').collect()
+        res = (
+            spark_string_matching.match_strings(
+                spark,
+                df=ddf,
+                string_column="matching_string",
+                row_number_column="id",
+                n_top=1500,
+                threshold=0.8,
+                n_gram=2,
+                min_document_frequency=2,
+                max_vocabulary_size=1500,
+            )
+                .select("i", "j")
+                .sort("j", "i")
+                .collect()
+        )
         assert len(res) == 1
         sources = [_[0] for _ in res]
         targets = [_[1] for _ in res]
@@ -45,16 +53,23 @@ class TestMatchStrings:
 
         ddf_delta = self.create_ddf(spark, data_delta)
         ddf_integrated = self.create_ddf(spark, data_integrated)
-        res = victim.match_strings(spark,
-                                   df=ddf_delta,
-                                   df2=ddf_integrated,
-                                   string_column='matching_string',
-                                   row_number_column='id',
-                                   n_top=1500,
-                                   threshold=0.8,
-                                   n_gram=2,
-                                   min_document_frequency=2,
-                                   max_vocabulary_size=1500).select('i', 'j').sort('i', 'j').collect()  # i is ingested
+        res = (
+            spark_string_matching.match_strings(
+                spark,
+                df=ddf_delta,
+                df2=ddf_integrated,
+                string_column="matching_string",
+                row_number_column="id",
+                n_top=1500,
+                threshold=0.8,
+                n_gram=2,
+                min_document_frequency=2,
+                max_vocabulary_size=1500,
+            )
+                .select("i", "j")
+                .sort("i", "j")
+                .collect()
+        )  # i is ingested
         assert len(res) == 5
         ingested = [_[0] for _ in res]
         integrated = [_[1] for _ in res]
@@ -67,15 +82,22 @@ class TestMatchStrings:
                 (2, 'ac'),
                 ]
         ddf = self.create_ddf(spark, data)
-        res = victim.match_strings(spark,
-                                   df=ddf,
-                                   string_column='matching_string',
-                                   row_number_column='id',
-                                   n_top=1500,
-                                   threshold=0.8,
-                                   n_gram=2,
-                                   min_document_frequency=2,
-                                   max_vocabulary_size=1500).select('i', 'j').sort('j', 'i').collect()
+        res = (
+            spark_string_matching.match_strings(
+                spark,
+                df=ddf,
+                string_column="matching_string",
+                row_number_column="id",
+                n_top=1500,
+                threshold=0.8,
+                n_gram=2,
+                min_document_frequency=2,
+                max_vocabulary_size=1500,
+            )
+                .select("i", "j")
+                .sort("j", "i")
+                .collect()
+        )
         assert len(res) == 0
 
     def test_matching_on_sparse_second_dataframe(self, spark):
@@ -91,14 +113,56 @@ class TestMatchStrings:
 
         ddf_delta = self.create_ddf(spark, data_delta)
         ddf_integrated = self.create_ddf(spark, data_integrated)
-        res = victim.match_strings(spark,
-                                   df=ddf_delta,
-                                   df2=ddf_integrated,
-                                   string_column='matching_string',
-                                   row_number_column='id',
-                                   n_top=1500,
-                                   threshold=0.8,
-                                   n_gram=2,
-                                   min_document_frequency=2,
-                                   max_vocabulary_size=1500).select('i', 'j').sort('j', 'i').collect()
+        res = (
+            spark_string_matching.match_strings(
+                spark,
+                df=ddf_delta,
+                df2=ddf_integrated,
+                string_column="matching_string",
+                row_number_column="id",
+                n_top=1500,
+                threshold=0.8,
+                n_gram=2,
+                min_document_frequency=2,
+                max_vocabulary_size=1500,
+            )
+                .select("i", "j")
+                .sort("j", "i")
+                .collect()
+        )
         assert len(res) == 0
+
+    def test_match_strings(self, spark):
+        df = spark.createDataFrame(
+            data=[
+                (0, 0, "brownthornton ebonybury abbott rue6 07172"),
+                (1, 1, "washington group katelynville debbie ville8 30336"),
+                (2, 2, "holloway llc dannyfort mann pines 48296"),
+                (3, 3, "owenthomas eastdarinfurt penny plaza 73947"),
+                (4, 4, "cobbnichols darrellmouth jeffery extension4 61626"),
+            ],
+            schema=StructType(
+                [
+                    StructField("name_index", IntegerType(), True),
+                    StructField("concatId", StringType(), True),
+                    StructField("matching_string", StringType(), True),
+                ]
+            ),
+        )
+
+        similarities = spark_string_matching.match_strings(
+            spark=spark,
+            df=df,
+            string_column="matching_string",
+            row_number_column="name_index",
+            n_top=1500,
+            threshold=0,
+            n_gram=2,
+            min_document_frequency=2,
+            max_vocabulary_size=1500,
+            matrix_chunks_rows=500,
+        )
+
+        # n records should result in n-1 th triangular number similarities.
+        # E.g. 5 records should result in 4+3+2+1=10 similarities.
+        assert similarities.count() == sum(range(df.count()))
