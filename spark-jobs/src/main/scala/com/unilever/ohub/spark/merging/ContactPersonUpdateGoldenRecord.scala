9d0fd073b2381db2a0ea6307b20ff18b55e3c04f
Vincent Huiberts <vhuiberts@xebia.com>
2019-01-15 08:54:37 +0100

    OHUB-2222 When optional column are not present in the input, suppress exceptions **reformatting due to running sbt command(test)

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/ContactPersonUpdateGoldenRecord.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/ContactPersonUpdateGoldenRecord.scala
@@ -2,11 +2,11 @@ package com.unilever.ohub.spark.merging
 
 import java.sql.Timestamp
 
-import com.unilever.ohub.spark.{DefaultConfig, SparkJobWithDefaultDbConfig}
+import com.unilever.ohub.spark.{ DefaultConfig, SparkJobWithDefaultDbConfig }
 import com.unilever.ohub.spark.domain.entity.ContactPerson
 import com.unilever.ohub.spark.storage.Storage
 import com.unilever.ohub.spark.DomainDataProvider
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.{ Dataset, SparkSession }
 import org.apache.spark.sql.functions._
 
 object ContactPersonUpdateGoldenRecord extends SparkJobWithDefaultDbConfig with GoldenRecordPicking[ContactPerson] {
@@ -19,11 +19,11 @@ object ContactPersonUpdateGoldenRecord extends SparkJobWithDefaultDbConfig with 
   // When it is decided to select golden record based on source instead of newest, remove
   // this override def pickGoldenRecord(...
   /**
-    * Get the newest contactPerson(based on dateUpdated, dateCreated and ohubUpdated) to mark as golden record.
-    * @param sourcePreference -- not used
-    * @param entities
-    * @return
-    */
+   * Get the newest contactPerson(based on dateUpdated, dateCreated and ohubUpdated) to mark as golden record.
+   * @param sourcePreference -- not used
+   * @param entities
+   * @return
+   */
   override def pickGoldenRecord(sourcePreference: Map[String, Int], entities: Seq[ContactPerson]): ContactPerson = {
     implicit def ordered: Ordering[Timestamp] = new Ordering[Timestamp] {
       def compare(x: Timestamp, y: Timestamp): Int = x compareTo y
