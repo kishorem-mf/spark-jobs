451774e40ebb6ea9c8a423cf73f9f32f0affa393
Vincent Huiberts <vhuiberts@xebia.com>
2019-05-27 12:43:34 +0200

    Fixed UT for exactMatching changes

--- sparktest/it-test/tests/test_operators.py
+++ sparktest/it-test/tests/test_operators.py
@@ -31,7 +31,7 @@ class TestOperators(object):
 
         assertDataframeCount(spark, "/usr/local/data/output/integrated/operators", 1000)
 
-        assertDataframeCount(spark, "/usr/local/data/output/integrated/operators_golden", 46)
+        assertDataframeCount(spark, "/usr/local/data/output/integrated/operators_golden", 769)
 
         # 46 unique ohubId's for DE
         assert (spark
