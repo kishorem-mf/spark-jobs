e83424d079885156fc2ea10dd59fb66afc5f7387
Vincent Huiberts <vhuiberts@xebia.com>
2019-05-28 08:02:46 +0200

    Added a cap to the groupSize used for merging. Currently set to 100.

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/OperatorCreateGoldenRecord.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/OperatorCreateGoldenRecord.scala
@@ -3,18 +3,31 @@ package com.unilever.ohub.spark.merging
 import com.unilever.ohub.spark.domain.DomainEntity.IngestionError
 import com.unilever.ohub.spark.domain.entity.Operator
 import com.unilever.ohub.spark.storage.Storage
-import com.unilever.ohub.spark.{DefaultConfig, SparkJobWithDefaultConfig}
-import org.apache.spark.sql.expressions.{Window, WindowSpec}
+import com.unilever.ohub.spark.{ DefaultConfig, SparkJobWithDefaultConfig }
+import org.apache.spark.sql.expressions.{ Window, WindowSpec }
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.{ DataFrame, Dataset, SparkSession }
 
 object OperatorCreateGoldenRecord extends SparkJobWithDefaultConfig {
+  val mergeGroupSizeCap = 100
+
   def transform(spark: SparkSession, operators: Dataset[Operator]): Dataset[Operator] = {
     import spark.implicits._
     val groupWindow = Window.partitionBy($"ohubId")
-    val activeOperators = operators.filter($"isActive").drop("additionalFields", "ingestionErrors")
 
-    setFieldsToLatestValue(spark, groupWindow, activeOperators)
+    val newestNotNullWindow = groupWindow.orderBy(
+      $"dateUpdated".desc_nulls_last,
+      $"dateCreated".desc_nulls_last,
+      $"ohubUpdated".desc
+    )
+
+    val mergeableOperators = operators
+      .filter($"isActive")
+      .withColumn("group_row_num", row_number().over(newestNotNullWindow))
+      .filter($"group_row_num" <= mergeGroupSizeCap)
+      .drop("additionalFields", "ingestionErrors")
+
+    setFieldsToLatestValue(spark, newestNotNullWindow, mergeableOperators, Seq("group_row_num"))
       .filter($"group_row_num" === 1)
       .drop("group_row_num")
       .withColumn("isGoldenRecord", lit(true))
@@ -23,15 +36,7 @@ object OperatorCreateGoldenRecord extends SparkJobWithDefaultConfig {
       .as[Operator]
   }
 
-  private[merging] def setFieldsToLatestValue(spark: SparkSession, groupWindow: WindowSpec, dataframe: DataFrame, excludeFields: Seq[String] = Seq()): DataFrame = {
-    import spark.implicits._
-
-    val newestNotNullWindow = groupWindow.orderBy(
-      $"dateUpdated".desc_nulls_last,
-      $"dateCreated".desc_nulls_last,
-      $"ohubUpdated".desc
-    )
-
+  private[merging] def setFieldsToLatestValue(spark: SparkSession, newestNotNullWindow: WindowSpec, dataframe: DataFrame, excludeFields: Seq[String] = Seq()): DataFrame = {
     // Set all columns of dataset on the first value of it's newestNotNullWindow
     dataframe.columns
       .filter(!excludeFields.contains(_))
@@ -40,7 +45,6 @@ object OperatorCreateGoldenRecord extends SparkJobWithDefaultConfig {
           op.withColumn(column, first(col(column), true).over(newestNotNullWindow))
         }
       )
-      .withColumn("group_row_num", row_number().over(newestNotNullWindow))
   }
 
   override def run(spark: SparkSession, config: DefaultConfig, storage: Storage): Unit = {
