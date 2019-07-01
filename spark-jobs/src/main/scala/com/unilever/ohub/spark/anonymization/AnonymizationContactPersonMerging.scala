c0be18d6ec2c4426612eeaf92e52efe66b988fd4
Nanne Baars <nanne.baars@unilever.com>
2019-07-01 08:12:34 +0200

    Do not fail when previous parquet file is not present

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/anonymization/AnonymizationContactPersonMerging.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/anonymization/AnonymizationContactPersonMerging.scala
@@ -1,20 +1,23 @@
 package com.unilever.ohub.spark.anonymization
 
 import com.unilever.ohub.spark.storage.Storage
-import com.unilever.ohub.spark.{SparkJob, SparkJobConfig}
-import org.apache.spark.sql.{Dataset, SparkSession}
+import com.unilever.ohub.spark.{ SparkJob, SparkJobConfig }
+import org.apache.hadoop.fs.{ FileSystem, Path }
+import org.apache.spark.sql.{ Dataset, SparkSession }
 import scopt.OptionParser
 
-case class AnonymizationMergingConfig(inputFile: String = "input-file",
-                                      previousIntegrated: String = "previous-integrated-file",
-                                      outputFile: String = "path-to-output-file"
-                                     ) extends SparkJobConfig
+case class AnonymizationMergingConfig(
+    inputFile: String = "input-file",
+    previousIntegrated: String = "previous-integrated-file",
+    outputFile: String = "path-to-output-file"
+) extends SparkJobConfig
 
 object AnonymizationMerging extends SparkJob[AnonymizationMergingConfig] {
 
-  def transform(newIdentifiers: Dataset[AnonymizedContactPersonIdentifier],
-                previousIntegrated: Dataset[AnonymizedContactPersonIdentifier]
-               ): Dataset[AnonymizedContactPersonIdentifier] = {
+  def transform(
+    newIdentifiers: Dataset[AnonymizedContactPersonIdentifier],
+    previousIntegrated: Dataset[AnonymizedContactPersonIdentifier]
+  ): Dataset[AnonymizedContactPersonIdentifier] = {
     previousIntegrated.union(newIdentifiers).dropDuplicates()
   }
 
@@ -23,13 +26,13 @@ object AnonymizationMerging extends SparkJob[AnonymizationMergingConfig] {
   override private[spark] def configParser(): OptionParser[AnonymizationMergingConfig] =
     new scopt.OptionParser[AnonymizationMergingConfig]("Anonymized merging") {
       head("merges anonymized into an integrated anonymized output file.", "1.0")
-      opt[String]("inputFile") required() action { (x, c) ⇒
+      opt[String]("inputFile") required () action { (x, c) ⇒
         c.copy(inputFile = x)
       } text "inputFile is a string property"
-      opt[String]("previousIntegrated") required() action { (x, c) ⇒
+      opt[String]("previousIntegrated") required () action { (x, c) ⇒
         c.copy(previousIntegrated = x)
       } text "previousIntegrated is a string property"
-      opt[String]("outputFile") required() action { (x, c) ⇒
+      opt[String]("outputFile") required () action { (x, c) ⇒
         c.copy(outputFile = x)
       } text "outputFile is a string property"
 
@@ -43,14 +46,21 @@ object AnonymizationMerging extends SparkJob[AnonymizationMergingConfig] {
     val newAnonymizedContactPersons = spark
       .read
       .option("header", true)
-      .csv(config.inputFile)
+      .option("delimiter", ";")
+      .csv(config.inputFile).select("hashedEmailAddress", "hashedMobileNumber")
       .as[AnonymizedContactPersonIdentifier]
 
-    var previousIntegrated = storage.readFromParquet[AnonymizedContactPersonIdentifier](config.previousIntegrated)
-    if (previousIntegrated.head(1).isEmpty) {
-      previousIntegrated = spark.createDataset[AnonymizedContactPersonIdentifier](Seq[AnonymizedContactPersonIdentifier]())
-    }
+    val previousIntegrated = if (dirExists(spark, config.previousIntegrated))
+      storage.readFromParquet[AnonymizedContactPersonIdentifier](config.previousIntegrated)
+    else
+      spark.createDataset[AnonymizedContactPersonIdentifier](Seq[AnonymizedContactPersonIdentifier]())
 
     storage.writeToParquet(transform(newAnonymizedContactPersons, previousIntegrated), config.outputFile)
+  }
+
+  def dirExists(spark: SparkSession, path: String): Boolean = {
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+    val p = new Path(path)
+    fs.exists(p) && fs.getFileStatus(p).isDirectory
   }
 }
