5889f4d3e5b4903760df1f286e107b4fca361c8b
Nanne Baars <nanne.baars@unilever.com>
2019-07-17 09:24:08 +0200

    OHUB-3034 Subscriptions to ACM with granular consent fields added

--- spark-jobs/src/test/scala/com/unilever/ohub/spark/export/acm/SubscriptionAcmConverterSpec.scala
+++ spark-jobs/src/test/scala/com/unilever/ohub/spark/export/acm/SubscriptionAcmConverterSpec.scala
@@ -10,11 +10,12 @@ class SubscriptionAcmConverterSpec extends FunSpec with TestSubscription with Ma
 
   describe("Subscription acm converter") {
     it("should convert a subscription correctly into an acm subscription") {
-      val subscription = defaultSubscription.copy(contactPersonOhubId = Some("DE~EMAKINA~1234"))
+      val subscription = defaultSubscription.copy(contactPersonOhubId = Some("a3a6208c-d7f6-44e2-80e2-f26d461f64c0"))
       val result = SUT.convert(subscription)
 
       val expectedAcmSubscription = AcmSubscription(
-        CONTACT_PARTY_ID = "DE~EMAKINA~1234",
+        SUBSCRIPTION_ID = "DE~EMAKINA~b3a6208c-d7f6-44e2-80e2-f26d461f64c0",
+        CP_LNKD_INTEGRATION_ID = "a3a6208c-d7f6-44e2-80e2-f26d461f64c0",
         COUNTRY_CODE = "DE",
         SUBSCRIBE_FLAG = "Y",
         SERVICE_NAME = "Newsletter",
@@ -23,7 +24,10 @@ class SubscriptionAcmConverterSpec extends FunSpec with TestSubscription with Ma
         SUBSCRIPTION_DATE = "2015/06/20 13:49:00",
         SUBSCRIPTION_CONFIRMED = "Y",
         SUBSCRIPTION_CONFIRMED_DATE = "2015/06/30 13:49:00",
-        FAIR_KITCHENS_SIGN_UP_TYPE = "pledger"
+        FAIR_KITCHENS_SIGN_UP_TYPE = "pledger",
+        COMMUNICATION_CHANNEL = "all",
+        SUBSCRIPTION_TYPE = "Newsletter",
+        DELETED_FLAG = "N"
       )
 
       result shouldBe expectedAcmSubscription
