bf88a566907650ad317d8fedfbf0bf7330ab5654
Nanne Baars <nanne.baars@unilever.com>
2019-08-09 21:37:19 +0200

    Remove PRODUCT_ID as there is absolutely no interest from ACM for that columns: so long innovation and trying to make this in a uniform way and give us a common primary key in both systems we could reason about. The naysayers do not want it so we remove it again

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/export/acm/ProductAcmConverter.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/export/acm/ProductAcmConverter.scala
@@ -15,8 +15,7 @@ object ProductAcmConverter extends Converter[Product, AcmProduct] with Transform
       MRDR_CODE = product.code,
       CREATED_AT = product.ohubCreated,
       UPDATED_AT = product.ohubUpdated,
-      DELETE_FLAG = booleanToYNConverter(!product.isActive),
-      PRODUCT_ID = product.concatId
+      DELETE_FLAG = booleanToYNConverter(!product.isActive)
     )
   }
 }
