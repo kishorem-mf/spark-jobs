e74e4b675f55d68419ad0d65e4afe8df93fc217d
Vincent Huiberts <vhuiberts@xebia.com>
2019-08-22 15:09:26 +0200

    OHUB-3069 Reviewing my own PR, some description couldn't hurt

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/export/Converter.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/export/Converter.scala
@@ -10,6 +10,11 @@ trait Converter[DomainType <: DomainEntity, OutboundType <: OutboundEntity] exte
 
   def getValue[T: TypeTag](name: String, transformFunction: TransformationFunction[T])(implicit input: DomainType, explain: Boolean): AnyRef = getValue(name, Some(transformFunction))
 
+  /**
+   * Function get the value from the implicitly present DomainType. If it's an Option, it's will either be unboxed or, when None, returned as an empty String.
+   *
+   * If the implicitly present boolean explain is set to true, an explanation is return as value instead of the actual value.
+   */
   def getValue[T: TypeTag](name: String, transformFunction: Option[TransformationFunction[T]] = None)(implicit input: DomainType, explain: Boolean): AnyRef = {
     val field = input.getClass.getDeclaredField(name)
     field.setAccessible(true)
