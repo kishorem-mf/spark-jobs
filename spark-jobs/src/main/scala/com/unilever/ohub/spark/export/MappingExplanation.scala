fc4a80f3387cd169de9183356d2c947e8c6b8878
Vincent Huiberts <vhuiberts@xebia.com>
2019-08-29 11:53:29 +0200

    Fixed a bug, where an Option[Boolean] would somehow result in a Object (so genericType was scala.Option<java.lang.Object>) In the case it is an Object, consult the typeTag tpe

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/export/MappingExplanation.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/export/MappingExplanation.scala
@@ -18,16 +18,23 @@ case class FieldMapping(fromEntity: String,
                         transformation: String = "")
 
 trait MappingExplanation[DomainType <: DomainEntity] extends TypeConversionFunctions {
-  protected def getExplanation[T: TypeTag](name: String, transformFunction: Option[TransformationFunction[T]] = None)(implicit input: DomainType, explain: Boolean): AnyRef = {
+  protected def getExplanation[T](name: String, transformFunction: Option[TransformationFunction[T]] = None)(implicit input: DomainType, explain: Boolean, tag: TypeTag[T]): AnyRef = {
     val genericDataTypePattern = "(scala.Option<)?([a-z]+\\.)+([A-Z][A-Za-z]+)>?".r
-
-    def getDataType(genericType: String) = genericDataTypePattern.replaceFirstIn(genericType, "$3")
-
-    def isDataTypeRequired(genericType: String) = !genericType.contains("scala.Option<")
 
     val field = input.getClass.getDeclaredField(name)
 
-    val dataType = getDataType(field.getGenericType.getTypeName)
+    def getDataType(): String = {
+      val typeName = genericDataTypePattern.replaceFirstIn(field.getGenericType.getTypeName, "$3") match {
+        case "Object" => tag.tpe.toString
+        case other => other
+      }
+      typeName.capitalize
+    }
+
+    def isDataTypeRequired(genericType: String) = !genericType.contains("scala.Option<")
+
+    val dataType = getDataType()
+    //    val dataType = getDataType(field.getGenericType.getTypeName)
 
     val fieldMapping = FieldMapping(
       fromEntity = input.getClass.getSimpleName,
