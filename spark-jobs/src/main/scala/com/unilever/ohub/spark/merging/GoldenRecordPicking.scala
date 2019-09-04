8e8872f2dcbd2c6d114a30019e49280759bd56d1
Reshma Patnaik <you@example.com>
2019-09-04 13:55:19 +0530

    [ENGINE] Fixed review comment

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/GoldenRecordPicking.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/GoldenRecordPicking.scala
@@ -14,36 +14,36 @@ trait GoldenRecordPicking[DomainType <: DomainEntity] {
       if (preference1 < preference2) o1
       else if (preference1 > preference2) o2
       else { // same source preference
-        var date1 = new Timestamp(0)
-        var date2 = new Timestamp(0)
+        var dateCreatedOrUpdated1 = new Timestamp(0)
+        var dateCreatedOrUpdated2 = new Timestamp(0)
 
         val isO2DateUpdatedEmpty = o2.dateUpdated.isEmpty
 
         if (o1.dateUpdated.isEmpty) {
           if (isO2DateUpdatedEmpty) {
-            date1 = o1.dateCreated.getOrElse(lowDate)
-            date2 = o2.dateCreated.getOrElse(lowDate)
+            dateCreatedOrUpdated1 = o1.dateCreated.getOrElse(lowDate)
+            dateCreatedOrUpdated2 = o2.dateCreated.getOrElse(lowDate)
           } else {
-            date1 = o1.dateCreated.getOrElse(lowDate)
-            date2 = o2.dateUpdated.getOrElse(lowDate)
+            dateCreatedOrUpdated1 = o1.dateCreated.getOrElse(lowDate)
+            dateCreatedOrUpdated2 = o2.dateUpdated.getOrElse(lowDate)
           }
         } else {
           if (isO2DateUpdatedEmpty) {
-            date1 = o1.dateUpdated.getOrElse(lowDate)
-            date2 = o2.dateCreated.getOrElse(lowDate)
+            dateCreatedOrUpdated1 = o1.dateUpdated.getOrElse(lowDate)
+            dateCreatedOrUpdated2 = o2.dateCreated.getOrElse(lowDate)
           } else {
             if (o1.dateUpdated.equals(o2.dateUpdated)) {
-              date1 = o1.dateCreated.getOrElse(lowDate)
-              date2 = o2.dateCreated.getOrElse(lowDate)
+              dateCreatedOrUpdated1 = o1.dateCreated.getOrElse(lowDate)
+              dateCreatedOrUpdated2 = o2.dateCreated.getOrElse(lowDate)
             } else {
-              date1 = o1.dateUpdated.getOrElse(lowDate)
-              date2 = o2.dateUpdated.getOrElse(lowDate)
+              dateCreatedOrUpdated1 = o1.dateUpdated.getOrElse(lowDate)
+              dateCreatedOrUpdated2 = o2.dateUpdated.getOrElse(lowDate)
             }
           }
         }
 
-        if (date1.equals(date2) && o1.isGoldenRecord) o1 // If it already was golden and dates are the same: prefer that one
-        else if (date1.after(date2)) o1
+        if (dateCreatedOrUpdated1.equals(dateCreatedOrUpdated2) && o1.isGoldenRecord) o1 // If it already was golden and dates are the same: prefer that one
+        else if (dateCreatedOrUpdated1.after(dateCreatedOrUpdated2)) o1
         else o2
       }
     })
