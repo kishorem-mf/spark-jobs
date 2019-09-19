6dc5217ed3d2482dd80db943269b8212822e43ef
shivajimadhusoodhan <shivajimadhusoodhan@gmail.com>
2019-09-19 14:52:23 +0530

    OHUB-3141/3142 Altered code due to Scala check style error (commit 2)

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/export/acm/AcmOutputWriters.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/export/acm/AcmOutputWriters.scala
@@ -27,7 +27,11 @@ object ContactPersonOutboundWriter extends ExportOutboundWriter[ContactPerson] w
 
   override def entityName(): String = "RECIPIENTS"
 
-  override def getDeletedOhubIdsFromPreviousIntegrated(spark: SparkSession, filteredChanges: Dataset[ContactPerson], previousIntegratedEntities: Dataset[ContactPerson], integratedEntities: Dataset[ContactPerson]) = {
+  override def getDeletedOhubIdsFromPreviousIntegrated(
+        spark: SparkSession,
+       filteredChanges: Dataset[ContactPerson],
+       previousIntegratedEntities: Dataset[ContactPerson],
+       integratedEntities: Dataset[ContactPerson]) : Dataset[ContactPerson]  = {
     val datasetWithTargetOhubId = getDeletedOhubIdsWithTargetId(spark, previousIntegratedEntities, integratedEntities)
 
     filteredChanges.unionByName(datasetWithTargetOhubId)
@@ -44,7 +48,11 @@ object OperatorOutboundWriter extends ExportOutboundWriter[Operator] with AcmOpt
 
   override def entityName(): String = "OPERATORS"
 
-  override def getDeletedOhubIdsFromPreviousIntegrated(spark: SparkSession, filteredChanges: Dataset[Operator], previousIntegratedEntities: Dataset[Operator], integratedEntities: Dataset[Operator]) = {
+  override def getDeletedOhubIdsFromPreviousIntegrated(
+      spark: SparkSession,
+      filteredChanges: Dataset[Operator],
+      previousIntegratedEntities: Dataset[Operator],
+      integratedEntities: Dataset[Operator]): Dataset[Operator]  = {
     val datasetWithTargetOhubId = getDeletedOhubIdsWithTargetId(spark, previousIntegratedEntities, integratedEntities)
     filteredChanges.unionByName(datasetWithTargetOhubId)
   }
