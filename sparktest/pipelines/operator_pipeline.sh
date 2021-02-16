16ce25e7b71dcbeeb8fb3aa7e0695bfba1b9a65d
Pratibha Janardhan <Pratibha.Janardhan@unilever.com>
2021-02-16 11:21:21 +0000

    Updated operator_pipeline.sh

--- sparktest/pipelines/operator_pipeline.sh
+++ sparktest/pipelines/operator_pipeline.sh
@@ -122,11 +122,8 @@ spark-submit   --class="com.unilever.ohub.spark.merging.OperatorUpdateChannelMap
 echo
 echo OperatorCreatePerfectGoldenRecord
 spark-submit   --class="com.unilever.ohub.spark.merging.OperatorCreatePerfectGoldenRecord" ${SPARK_JOBS_JAR} \
-               --driver-cores 2 \
-               --driver-memory 4G \
-               --num-executors 10 \
-               --executor-cores 5 \
-               --executor-memory 4G \ 
+               --driver-memory=4G \
+               --executor-memory=4G \ 
                --inputFile=${DATA_OPERATORS_INTEGRATED_OUTPUT} \
                --outputFile=${DATA_OPERATORS_CREATED_GOLDEN_RECORDS}
 
