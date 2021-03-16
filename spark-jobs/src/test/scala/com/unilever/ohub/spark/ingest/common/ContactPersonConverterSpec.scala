f5904dda65f37b141e106cfe6774bbeb89046967
Pratibha <pratibha.janardhan@unilever.com>
2021-03-16 13:19:40 +0530

    correct tests

--- spark-jobs/src/test/scala/com/unilever/ohub/spark/ingest/common/ContactPersonConverterSpec.scala
+++ spark-jobs/src/test/scala/com/unilever/ohub/spark/ingest/common/ContactPersonConverterSpec.scala
@@ -94,7 +94,7 @@ class ContactPersonConverterSpec extends CsvDomainGateKeeperSpec[ContactPerson] 
             subscriptionsList = None,
             executeRightToBeForgotten = None,
             hasTelephoneSuppressed = None,
-            emailOptOutDate = Some(Timestamp.valueOf("2015-10-30 14:23:02.0")),
+            emailOptOutDate = None,
             startWorkDate = None,
             endWorkDate = None,
             doNotCall = None,
