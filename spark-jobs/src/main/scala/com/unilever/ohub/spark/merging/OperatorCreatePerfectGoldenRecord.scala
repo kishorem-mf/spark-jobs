8ce9cf20f9b67c136485d5a28e5c882d09dbdc53
Pratibha <pratibha.janardhan@unilever.com>
2021-04-24 13:54:37 +0530

    source priority

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/OperatorCreatePerfectGoldenRecord.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/OperatorCreatePerfectGoldenRecord.scala
@@ -2,15 +2,17 @@ package com.unilever.ohub.spark.merging
 
 import com.unilever.ohub.spark.domain.entity.OperatorGolden
 import com.unilever.ohub.spark.storage.Storage
-import com.unilever.ohub.spark.DefaultConfig
+import com.unilever.ohub.spark.{Constants, DefaultConfig}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.expressions.{ Window, WindowSpec }
+import org.apache.spark.sql.expressions.{Window, WindowSpec}
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StructType
 
 object OperatorCreatePerfectGoldenRecord extends BaseMerging[OperatorGolden] {
   // scalastyle:off
   override def run(spark: SparkSession, config: DefaultConfig, storage: Storage): Unit = {
+    import spark.implicits._
     val entity = storage.readFromParquet[OperatorGolden](config.inputFile)
     val entityAllCountries = entity
       .filter(!col("countryCode").isin("CH", "AT", "DE", "GB", "IE"))
@@ -19,50 +21,40 @@ object OperatorCreatePerfectGoldenRecord extends BaseMerging[OperatorGolden] {
     val entityUkiCountries = entity
       .filter(col("countryCode").isin("GB", "IE"))
 
-    val groupWin = Window.partitionBy(col("ohubId"))
-
-    val orderByWindow = groupWin.orderBy(
-      col("sourcePriority").desc_nulls_last,
+    val sourceListDachDf = spark.createDataFrame(
+      spark.sparkContext.parallelize(Constants.sourceListDach),
+      StructType(Constants.schemaSourcePriority)
+    )
+    val sourceListUkiDf = spark.createDataFrame(
+      spark.sparkContext.parallelize(Constants.sourceListUki),
+      StructType(Constants.schemaSourcePriority)
+    )
+    val groupPriorityWindow = Window.partitionBy("ohubId","priority")
+    val orderByDateWindow = groupPriorityWindow.orderBy(
       when(col("dateUpdated").isNull, col("dateCreated"))
         .otherwise(col("dateUpdated")).desc_nulls_last,
       col("dateCreated").desc_nulls_last,
       col("ohubUpdated").desc
     )
-    val nonPreferredCol=Seq("id", "creationTimestamp", "concatId", "countryCode", "customerType", "dateCreated",
-      "dateUpdated", "isGoldenRecord", "ohubId", "sourceEntityId", "sourceName", "ohubCreated", "ohubUpdated",
-      "annualTurnover", "averagePrice", "averageRating", "beveragePurchasePotential", "buildingSquareFootage",
-      "chainId", "chainName", "cookingConvenienceLevel", "countryName", "daysOpen", "distributorName",
-      "distributorOperatorId", "hasDirectMailOptIn", "hasDirectMailOptOut", "hasEmailOptIn", "hasEmailOptOut",
-      "hasFaxOptIn", "hasFaxOptOut", "hasGeneralOptOut", "hasMobileOptIn", "hasMobileOptOut", "hasTelemarketingOptIn",
-      "hasTelemarketingOptOut", "headQuarterAddress", "headQuarterCity", "headQuarterPhoneNumber", "headQuarterState",
-      "headQuarterZipCode", "houseNumberExtension", "isNotRecalculatingOtm", "isOpenOnFriday", "isOpenOnMonday",
-      "isOpenOnSaturday", "isOpenOnSunday", "isOpenOnThursday", "isOpenOnTuesday", "isOpenOnWednesday",
-      "isPrivateHousehold", "kitchenType", "menuKeywords", "netPromoterScore", "numberOfProductsFittingInMenu",
-      "numberOfReviews", "oldIntegrationId", "operatorLeadScore", "otm", "otmEnteredBy", "potentialSalesValue",
-      "salesRepresentative", "totalDishes", "totalLocations", "totalStaff", "vat", "wayOfServingAlcohol", "webUpdaterId",
-      "weeksClosed", "yearFounded", "localChannel", "channelUsage", "socialCommercial", "strategicChannel", "globalChannel",
-      "globalSubChannel", "ufsClientNumber", "department", "crmId", "distributionChannel", "division", "salesOrgId",
-      "closingTimeWorkingDay", "openingTimeWorkingDay", "preferredVisitDays", "preferredVisitStartTime",
-      "preferredVisitEndTime", "preferredDeliveryDays", "preferredVisitWeekOfMonth", "hasWebshopRegistration",
-      "hasLoyaltyManagementOptIn", "monthlyFoodSpend", "customerHierarchyLevel3", "customerHierarchyLevel4", "customerHierarchyLevel5",
-      "mixedOrUfs", "salesGroupKey", "salesOfficeKey", "industryKey", "salesDistrict", "customerGroup", "languageKey",
-      "sapCustomerId", "isIndirectAccount", "hasWebshopAccount", "daysOpenPerWeek", "hasOutsideSeatings", "hasTakeAway",
-      "hasHomeDelivery", "numberOfSeats", "twitterUrl", "facebookUrl", "instagramUrl", "numberOfChildSites", "lastWebLoginDate",
-      "lastOpenedNewsletterDate", "subcribedToUfsNewsletter", "subcribedToIceCreamNewsletter", "last3OnlineEvents",
-      "totalFacebookCampaignsClicked", "livechatActivities", "totalLoyaltyRewardsBalancePoints", "loyaltyRewardsBalanceUpdatedDate",
-      "unileverNowClassification", "hasTelephoneSuppressed", "caterlystPotentialTurnover", "salesRepEstimatedPotentialTurnover",
-      "preferredCommunicationMethod", "hasPermittedToShareSsd", "isOkWithVisits", "customerFlag", "mergeWith", "hasToBeMerged",
-      "specialPrice", "categoryAttributes", "accountOwner", "numberOfTillPoints", "keyDecisionMaker", "seasonCloseTime", "poNumber",
-      "otmOohCalculated", "otmUfsCalculated", "eOtm", "relativeLeadScore", "hasPermittedToOrder", "ofsValue", "routeToMarketIceCreamCategory")
+    val groupOhubIdWindow = Window.partitionBy("ohubId")
+    val orderBySourceWindow = groupOhubIdWindow.orderBy(
+      col("priority").asc_nulls_last
+    )
 
-    val dach_sources = List("CRM", "FUZZIT", "SAP_SIRIUS", "CONSESSIONAIRES_AT", "ECCO_JAEGER", "FRIGEMO", "GMUR", "GRUENENFELDER",
-                            "MULTIFOOD", "VERMO", "WALKER", "SWISS_GATEWAY")
-    val uki_sources = List("CRM", "CATERLYST", "SAP_SIRIUS")
-
-    val outputGoldenDach=transform(spark,entityDachCountries,orderByWindow,dach_sources,nonPreferredCol)
-    val outputGoldenUki=transform(spark,entityUkiCountries,orderByWindow,uki_sources,nonPreferredCol)
+    val nonPreferredCol=Constants.nonPreferredColumns
+    val dach_df=entityDachCountries.toDF()
+      .join(sourceListDachDf,Seq("sourceName"),"left")
+    val uki_df=entityUkiCountries.toDF()
+      .join(sourceListUkiDf,Seq("sourceName"),"left")
+    val outputDach=transform(spark,dach_df,orderByDateWindow,nonPreferredCol)
+    val outputUki=transform(spark,uki_df,orderByDateWindow,nonPreferredCol)
+    val outputGoldenDach=transform(spark,outputDach,orderBySourceWindow,nonPreferredCol)
+      .drop("priority")
+      .as[OperatorGolden]
+    val outputGoldenUki=transform(spark,outputUki,orderBySourceWindow,nonPreferredCol)
+      .drop("priority")
+      .as[OperatorGolden]
     val outputGoldenOthers=transform(spark,entityAllCountries)
-
     val dachUkiGolden=outputGoldenDach.unionByName(outputGoldenUki)
     val allDataGolden=dachUkiGolden.unionByName(outputGoldenOthers)
 
