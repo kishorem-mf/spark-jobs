790d9f5b4e5904b57b9375abc95cee331f6619b2
Pratibha Janardhan <Pratibha.Janardhan@unilever.com>
2021-05-26 07:49:35 +0000

    Solved date updated problem

--- spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/OperatorStitchUpdateMapping.scala
+++ spark-jobs/src/main/scala/com/unilever/ohub/spark/merging/OperatorStitchUpdateMapping.scala
@@ -2,9 +2,12 @@ package com.unilever.ohub.spark.merging
 
 import com.unilever.ohub.spark.domain.entity.Operator
 import com.unilever.ohub.spark.storage.Storage
-import com.unilever.ohub.spark.{SparkJob, SparkJobConfig}
-import org.apache.spark.sql.{DataFrame,Dataset, SparkSession}
+import com.unilever.ohub.spark.{Constants,SparkJob, SparkJobConfig}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import scopt.OptionParser
+import java.sql.Date
+
+case class Record(dateUpdated:  Date, concat_source: String, concat_caterlyst: String) extends scala.Product
 
 case class OperatorStitchUpdateMappingConfig(
                                                udlLookupMappingsInputFile: String = "udl-lookup-mappings-input-file",
@@ -51,9 +54,21 @@ object OperatorStitchUpdateMapping extends SparkJob[OperatorStitchUpdateMappingC
     }
 
   override def run(spark: SparkSession, config: OperatorStitchUpdateMappingConfig, storage: Storage): Unit = {
+    import spark.implicits._
     val operators = storage.readFromParquet[Operator](config.operatorsInputFile)
-    val udl_mappings = storage.readFromCsv(config.udlLookupMappingsInputFile,";",true)
-    val transformed = transform(spark, operators.toDF(),udl_mappings)
+    val textRDD = spark.sparkContext.textFile(config.udlLookupMappingsInputFile)
+    val format=new java.text.SimpleDateFormat("yyyy-MM-dd")
+    val records = textRDD.map {
+      line =>
+        val col = line.split(";")
+        Record(new java.sql.Date(format.parse(col(0)).getTime()), col(1), col(2))
+    }
+
+    val result = records.groupBy(_.concat_source).map { _._2.reduce {
+      (r1: Record, r2: Record) => if (r1.dateUpdated.after(r2.dateUpdated)) r1 else r2
+    }}
+    val dfNoDuplicateSources = spark.createDataFrame(result)
+    val transformed = transform(spark, operators.toDF(),dfNoDuplicateSources)
 
     storage.writeToParquet(transformed, config.outputFile)
   }
